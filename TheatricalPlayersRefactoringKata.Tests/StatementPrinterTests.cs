b3c3e7e45409a8e3b0f39669e7ba229f78f0053d
Julio Becker <julio10becker@yahoo.com.br>
2022-04-28 15:30:38 -0300

    add .vs folder to gitignore / add Type class

--- TheatricalPlayersRefactoringKata.Tests/StatementPrinterTests.cs
+++ TheatricalPlayersRefactoringKata.Tests/StatementPrinterTests.cs
@@ -13,9 +13,9 @@ public class StatementPrinterTests
     public void TestStatementExampleLegacy()
     {
         var plays = new Dictionary<string, Play>();
-        plays.Add("hamlet", new Play("Hamlet", 4024, "tragedy"));
-        plays.Add("as-like", new Play("As You Like It", 2670, "comedy"));
-        plays.Add("othello", new Play("Othello", 3560, "tragedy"));
+        plays.Add("hamlet", new Play("Hamlet", 4024, new Type("tragedy")));
+        plays.Add("as-like", new Play("As You Like It", 2670, new Type("comedy")));
+        plays.Add("othello", new Play("Othello", 3560, new Type("tragedy")));
 
         Invoice invoice = new Invoice(
             "BigCo",
@@ -38,12 +38,12 @@ public class StatementPrinterTests
     public void TestTextStatementExample()
     {
         var plays = new Dictionary<string, Play>();
-        plays.Add("hamlet", new Play("Hamlet", 4024, "tragedy"));
-        plays.Add("as-like", new Play("As You Like It", 2670, "comedy"));
-        plays.Add("othello", new Play("Othello", 3560, "tragedy"));
-        plays.Add("henry-v", new Play("Henry V", 3227, "history"));
-        plays.Add("john", new Play("King John", 2648, "history"));
-        plays.Add("richard-iii", new Play("Richard III", 3718, "history"));
+        plays.Add("hamlet", new Play("Hamlet", 4024, new Type("tragedy")));
+        plays.Add("as-like", new Play("As You Like It", 2670, new Type("comedy")));
+        plays.Add("othello", new Play("Othello", 3560, new Type("tragedy")));
+        plays.Add("henry-v", new Play("Henry V", 3227, new Type("history")));
+        plays.Add("john", new Play("King John", 2648, new Type("history")));
+        plays.Add("richard-iii", new Play("Richard III", 3718, new Type("history")));
 
         Invoice invoice = new Invoice(
             "BigCo",
