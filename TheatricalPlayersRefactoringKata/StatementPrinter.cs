1005feccd6e857b33d013ca63dd81904420d7904
Julio Cesar Becker Vasconselos <julio.vasconselos@opentechgr.com.br>
2022-04-28 17:33:41 -0300

    add CalcAmount method to Type class

--- TheatricalPlayersRefactoringKata/StatementPrinter.cs
+++ TheatricalPlayersRefactoringKata/StatementPrinter.cs
@@ -22,22 +22,7 @@ public class StatementPrinter
             if (lines < minLines) lines = minLines;
             if (lines > maxLines) lines = maxLines;
             var thisAmount = lines * 10;
-            switch (play.Type.Name) 
-            {
-                case "tragedy":
-                    if (perf.Audience > 30) {
-                        thisAmount += 1000 * (perf.Audience - 30);
-                    }
-                    break;
-                case "comedy":
-                    if (perf.Audience > 20) {
-                        thisAmount += 10000 + 500 * (perf.Audience - 20);
-                    }
-                    thisAmount += 300 * perf.Audience;
-                    break;
-                default:
-                    throw new Exception("unknown type: " + play.Type);
-            }
+            thisAmount += play.Type.CalcAmount(perf);
             // add volume credits
             volumeCredits += Math.Max(perf.Audience - 30, 0);
             // add extra credit for every ten comedy attendees
--- TheatricalPlayersRefactoringKata/Type.cs
+++ TheatricalPlayersRefactoringKata/Type.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace TheatricalPlayersRefactoringKata;
 
 public class Type
@@ -8,5 +10,26 @@ public class Type
 
     public Type(string name) {
         this._name = name;
+    }
+
+    public int CalcAmount(Performance perf){
+        int amount = -1;
+        switch (this._name) 
+        {
+            case "tragedy":
+                if (perf.Audience > 30) {
+                    amount = 1000 * (perf.Audience - 30);
+                }
+                break;
+            case "comedy":
+                amount = 300 * perf.Audience;
+                if (perf.Audience > 20) {
+                    amount += 10000 + 500 * (perf.Audience - 20);
+                }
+                break;
+            default:
+                throw new Exception("unknown type: " + this._name);
+        }
+        return amount;
     }
 }
