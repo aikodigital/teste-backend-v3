7f9e32853cff8f9e4a11eea2528b3576a9233d03
Julio Becker <julio10becker@yahoo.com.br>
2022-05-02 13:50:55 -0300

    Add PrintXML method (not ready). Add TestXmlStatement test for future validations on XML invoices.

--- TheatricalPlayersRefactoringKata.Tests/StatementPrinterTests.cs
+++ TheatricalPlayersRefactoringKata.Tests/StatementPrinterTests.cs
@@ -63,4 +63,35 @@ public class StatementPrinterTests
 
         Approvals.Verify(result);
     }
+
+    [Fact]
+    [UseReporter(typeof(DiffReporter))]
+    public void TestXmlStatementExample()
+    {
+        var plays = new Dictionary<string, Play>();
+        plays.Add("hamlet", new Play("Hamlet", 4024, new Type("tragedy")));
+        plays.Add("as-like", new Play("As You Like It", 2670, new Type("comedy")));
+        plays.Add("othello", new Play("Othello", 3560, new Type("tragedy")));
+        plays.Add("henry-v", new Play("Henry V", 3227, new Type("history")));
+        plays.Add("john", new Play("King John", 2648, new Type("history")));
+        plays.Add("richard-iii", new Play("Richard III", 3718, new Type("history")));
+
+        Invoice invoice = new Invoice(
+            "BigCo",
+            new List<Performance>
+            {
+                new Performance("hamlet", 55),
+                new Performance("as-like", 35),
+                new Performance("othello", 40),
+                new Performance("henry-v", 20),
+                new Performance("john", 39),
+                new Performance("henry-v", 20)
+            }
+        );
+
+        StatementPrinter statementPrinter = new StatementPrinter();
+        var result = statementPrinter.PrintXML(invoice, plays);
+
+        Approvals.Verify(result);
+    }
 }
--- TheatricalPlayersRefactoringKata/StatementPrinter.cs
+++ TheatricalPlayersRefactoringKata/StatementPrinter.cs
@@ -1,19 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Xml;
 
 namespace TheatricalPlayersRefactoringKata;
 
 public class StatementPrinter
 {
+    int minLines = 1000;
+    int maxLines = 4000;
+    CultureInfo cultureInfo = new CultureInfo("en-US");
+
     public string Print(Invoice invoice, Dictionary<string, Play> plays)
     {
-        var minLines = 1000;
-        var maxLines = 4000;
         var totalAmount = 0;
         var volumeCredits = 0;
         var result = string.Format("Statement for {0}\n", invoice.Customer);
-        CultureInfo cultureInfo = new CultureInfo("en-US");
 
         foreach(var perf in invoice.Performances) 
         {
@@ -32,5 +34,27 @@ public class StatementPrinter
         result += String.Format(cultureInfo, "Amount owed is {0:C}\n", Convert.ToDecimal(totalAmount / 100f));
         result += String.Format("You earned {0} credits\n", volumeCredits);
         return result;
+    }
+
+    public string PrintXML(Invoice invoice, Dictionary<string, Play> plays)
+    {
+        //Declare a new XMLDocument object
+        XmlDocument result = new XmlDocument();
+        XmlDeclaration xmlDeclaration = result.CreateXmlDeclaration("1.0", "utf-8", null);
+        result.AppendChild(xmlDeclaration);
+
+        //root element
+        XmlElement root = result.CreateElement("Statement");
+        root.SetAttribute("xmlns:xsi", "http://www.w3.org/2001/XMLSchema-instance");
+        root.SetAttribute("xmlns:xsd", "http://www.w3.org/2001/XMLSchema");
+        result.AppendChild(root);
+
+        //Customer node
+        XmlElement customerNode = result.CreateElement("Customer");
+        root.AppendChild(customerNode);
+        XmlText customerText = result.CreateTextNode(invoice.Customer);
+        customerNode.AppendChild(customerText);
+
+        return result.OuterXml;
     }
 }
