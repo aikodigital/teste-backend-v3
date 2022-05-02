50014d1c25ae6742041db75a8485ec013501aead
João Victor Viana Pacheco <joaovitorsl2012@hotmail.com>
2022-05-02 15:48:57 -0300

    Refact.2 * Implement correctly HistoryPerformance

--- TheatricalPlayersRefactoringKata/Performances/HistoryPerformance.cs
+++ TheatricalPlayersRefactoringKata/Performances/HistoryPerformance.cs
@@ -8,8 +8,19 @@ public class HistoryPerformance : BasePerformance
     public override int Audience { get; set; }
     public override IPlay Play { get; set; }
 
-    public override int CalculateAmmount() => throw new NotImplementedException();
-    public override int CalculateCredits() => throw new NotImplementedException();
+    public override int CalculateAmmount()
+    {
+        var tragedy = new TragedyPerformance(Play, Audience);
+
+        var comedy = new ComedyPerformance(Play, Audience);
+
+        return tragedy.CalculateAmmount() + comedy.CalculateAmmount();
+    }
+
+    public override int CalculateCredits()
+    {
+        return 0;
+    }
 
     public HistoryPerformance(IPlay play, int audience) : base(play, audience)
     {
