c52ac662e53055d2599058473f55b4206a1a472c
Victor Jaete <jaetev@outlook.com>
2024-08-15 13:33:13 -0300

    feat: implement xml printer

--- TheatricalPlayersRefactoringKata/StatementPrinter.cs
+++ TheatricalPlayersRefactoringKata/StatementPrinter.cs
@@ -6,47 +6,100 @@ namespace TheatricalPlayersRefactoringKata;
 
 public class StatementPrinter
 {
-    public string Print(Invoice invoice, Dictionary<string, Play> plays)
+    public const string TEXT_MODE = "text";
+    public const string XML_MODE = "xml";
+
+    public const int MIN_AUDIENCE_FOR_CREDITS = 30;
+    
+    public string Print(Invoice invoice, Dictionary<string, Play> plays, string printMode)
     {
         var totalAmount = 0;
         var volumeCredits = 0;
         var result = string.Format("Statement for {0}\n", invoice.Customer);
         CultureInfo cultureInfo = new CultureInfo("en-US");
 
-        foreach(var perf in invoice.Performances) 
-        {
+        if (printMode == XML_MODE){
+            result = "\ufeff<?xml version=\"1.0\" encoding=\"utf-8\"?>\n";
+            result += "<Statement xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">\n";
+            result += $"  <Customer>{invoice.Customer}</Customer>\n";
+            result +=  "  <Items>\n";
+        }
+
+        foreach(var perf in invoice.Performances){
+            int itemCredits;
             var play = plays[perf.PlayId];
             var lines = play.Lines;
             if (lines < 1000) lines = 1000;
             if (lines > 4000) lines = 4000;
-            var thisAmount = lines * 10;
-            switch (play.Type) 
+            var itemAmount = lines * 10;
+            switch (play.Genre) 
             {
-                case "tragedy":
-                    if (perf.Audience > 30) {
-                        thisAmount += 1000 * (perf.Audience - 30);
-                    }
+                case Genres.TRAGEDY:
+                    itemAmount = CalculateTragedyAmount(perf, itemAmount);
                     break;
-                case "comedy":
-                    if (perf.Audience > 20) {
-                        thisAmount += 10000 + 500 * (perf.Audience - 20);
-                    }
-                    thisAmount += 300 * perf.Audience;
+                case Genres.COMEDY:
+                    itemAmount = CalculateComedyAmount(perf, itemAmount);
+                    break;
+                case Genres.HISTORY:
+                    itemAmount = CalculateHistoryAmount(perf, itemAmount);
                     break;
                 default:
-                    throw new Exception("unknown type: " + play.Type);
+                    throw new Exception("unknown type: " + play.Genre);
             }
-            // add volume credits
-            volumeCredits += Math.Max(perf.Audience - 30, 0);
+            // add item credits
+            itemCredits = Math.Max(perf.Audience - MIN_AUDIENCE_FOR_CREDITS, 0);
             // add extra credit for every ten comedy attendees
-            if ("comedy" == play.Type) volumeCredits += (int)Math.Floor((decimal)perf.Audience / 5);
+            if (play.Genre == Genres.COMEDY) itemCredits += (int)Math.Floor((decimal)perf.Audience / 5);
 
+            // add volume credits
+            volumeCredits += itemCredits;
             // print line for this order
-            result += String.Format(cultureInfo, "  {0}: {1:C} ({2} seats)\n", play.Name, Convert.ToDecimal(thisAmount / 100), perf.Audience);
-            totalAmount += thisAmount;
+            switch (printMode){
+                case TEXT_MODE:
+                    result += String.Format(cultureInfo, "  {0}: {1:C} ({2} seats)\n", play.Name, (decimal)itemAmount / 100, perf.Audience);
+                    break;
+                case XML_MODE:
+                    result +=  "    <Item>\n";
+                    result += String.Format(cultureInfo, 
+                               "      <AmountOwed>{0}</AmountOwed>\n", (decimal)itemAmount / 100);
+                    result += $"      <EarnedCredits>{itemCredits}</EarnedCredits>\n";
+                    result += $"      <Seats>{perf.Audience}</Seats>\n";    
+                    result += "    </Item>\n";
+                    break;
+            }
+            totalAmount += itemAmount;
         }
-        result += String.Format(cultureInfo, "Amount owed is {0:C}\n", Convert.ToDecimal(totalAmount / 100));
-        result += String.Format("You earned {0} credits\n", volumeCredits);
+        switch (printMode){
+            case TEXT_MODE:
+                result += String.Format(cultureInfo, "Amount owed is {0:C}\n", (decimal)totalAmount / 100);
+                result += String.Format("You earned {0} credits\n", volumeCredits);
+                break;
+            case XML_MODE:
+                result += "  </Items>\n";
+                result += String.Format(cultureInfo, "  <AmountOwed>{0}</AmountOwed>\n", (decimal)totalAmount / 100);
+                result += $"  <EarnedCredits>{volumeCredits}</EarnedCredits>\n";
+                result += "</Statement>";
+                break;
+        }
+        
         return result;
+    }
+
+    public int CalculateTragedyAmount(Performance perf, int amount){
+        if(perf.Audience < 30) return amount;
+        return amount + 1000 * (perf.Audience - 30);
+    }
+    
+    
+    public int CalculateComedyAmount(Performance perf, int amount){
+        var newAmount = amount;
+        if (perf.Audience > 20) {
+            newAmount += 10000 + 500 * (perf.Audience - 20);
+        }
+        return newAmount + 300 * perf.Audience;
+    }
+
+    public int CalculateHistoryAmount(Performance perf, int amount){
+        return CalculateTragedyAmount(perf, amount) + CalculateComedyAmount(perf, amount);
     }
 }
