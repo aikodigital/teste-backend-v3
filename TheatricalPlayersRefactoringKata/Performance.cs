6102f274dd220fb196f85a314bf82c251a95af90
Victor Jaete <jaetev@outlook.com>
2024-08-15 12:25:47 -0300

    add: Genre to performance and play

--- TheatricalPlayersRefactoringKata/Performance.cs
+++ TheatricalPlayersRefactoringKata/Performance.cs
@@ -4,14 +4,17 @@ public class Performance
 {
     private string _playId;
     private int _audience;
+    private string _playGenre;
 
     public string PlayId { get => _playId; set => _playId = value; }
     public int Audience { get => _audience; set => _audience = value; }
+    public string PlayGenre { get => _playGenre; set => _playGenre = value; }
 
-    public Performance(string playID, int audience)
+    public Performance(string playID, int audience, string playGenre)
     {
         this._playId = playID;
         this._audience = audience;
+        this._playGenre = playGenre;
     }
 
 }
