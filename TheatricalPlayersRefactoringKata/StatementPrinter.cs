50c7361e4e55bf7b46691db5bb3568a111bb7c81
Cesar Eduardo <123430152+ceza01@users.noreply.github.com>
2024-08-16 19:46:34 -0300

    refactor: alterar variáveis para 'decimal'

--- TheatricalPlayersRefactoringKata/StatementPrinter.cs
+++ TheatricalPlayersRefactoringKata/StatementPrinter.cs
@@ -8,7 +8,7 @@ public class StatementPrinter
 {
     public string Print(Invoice invoice, Dictionary<string, Play> plays)
     {
-        var totalAmount = 0;
+        decimal totalAmount = 0;
         var volumeCredits = 0;
         var result = string.Format("Statement for {0}\n", invoice.Customer);
         CultureInfo cultureInfo = new CultureInfo("en-US");
@@ -19,7 +19,7 @@ public class StatementPrinter
             var lines = play.Lines;
             if (lines < 1000) lines = 1000;
             if (lines > 4000) lines = 4000;
-            var thisAmount = lines * 10;
+            decimal thisAmount = lines * 10;
             switch (play.Type)
             {
                 case "tragedy":
@@ -40,15 +40,15 @@ public class StatementPrinter
             if ("comedy" == play.Type) volumeCredits += (int)Math.Floor((decimal)perf.Audience / 5);
 
             // print line for this order
-            result += String.Format(cultureInfo, "  {0}: {1:C} ({2} seats)\n", play.Name, Convert.ToDecimal(thisAmount / 100), perf.Audience);
+            result += String.Format(cultureInfo, "  {0}: {1:C} ({2} seats)\n", play.Name, thisAmount / 100, perf.Audience);
             totalAmount += thisAmount;
         }
-        result += String.Format(cultureInfo, "Amount owed is {0:C}\n", Convert.ToDecimal(totalAmount / 100));
+        result += String.Format(cultureInfo, "Amount owed is {0:C}\n", totalAmount / 100);
         result += String.Format("You earned {0} credits\n", volumeCredits);
         return result;
     }
 
-    private static int CalculateComedyAmount(Performance perf, int thisAmount)
+    private static decimal CalculateComedyAmount(Performance perf, decimal thisAmount)
     {
         if (perf.Audience > 20)
         {
@@ -58,7 +58,7 @@ public class StatementPrinter
         return thisAmount;
     }
 
-    private static int CalculateTragedyAmount(Performance perf, int thisAmount)
+    private static decimal CalculateTragedyAmount(Performance perf, decimal thisAmount)
     {
         if (perf.Audience > 30)
         {
@@ -68,11 +68,11 @@ public class StatementPrinter
         return thisAmount;
     }
 
-    private static int CalculateHistoryAmount(Performance perf, int thisAmount)
+    private static decimal CalculateHistoryAmount(Performance perf, decimal thisAmount)
     {
-        int tragedyAmount = CalculateTragedyAmount(perf, thisAmount);
+        decimal tragedyAmount = CalculateTragedyAmount(perf, thisAmount);
 
-        int comedyAmount = CalculateComedyAmount(perf, thisAmount);
+        decimal comedyAmount = CalculateComedyAmount(perf, thisAmount);
 
         thisAmount = tragedyAmount + comedyAmount;
         return thisAmount;
