67531c9daa18c1ebad97883c427e14d86d5184b8
Hugo Vidal <vidalshugo1@gmai.com>
2024-08-16 16:53:33 -0300

    Modularizacao do codigo seguindo os principios SOLID

--- TheatricalPlayersRefactoringKata/StatementPrinter.cs
+++ TheatricalPlayersRefactoringKata/StatementPrinter.cs
@@ -1,72 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
 
-namespace TheatricalPlayersRefactoringKata;
-
-public class StatementPrinter
+namespace TheatricalPlayersRefactoringKata
 {
-    public string Print(Invoice invoice, Dictionary<string, Play> plays)
+    public class StatementPrinter
     {
-        var totalAmount = 0;
-        var volumeCredits = 0;
-        var result = string.Format("Statement for {0}\n", invoice.Customer);
-        CultureInfo cultureInfo = new CultureInfo("en-US");
+        private readonly CultureInfo _cultureInfo = CultureInfo.GetCultureInfo("en-US");
 
-        foreach (var perf in invoice.Performances)
+        private readonly CalculatorPrice calculatorPrice = new CalculatorPrice();
+
+        public string Print(Invoice invoice, Dictionary<string, Play> plays)
         {
-            var play = plays[perf.PlayId];
-            var lines = play.Lines;
-            if (lines < 1000) lines = 1000;
-            if (lines > 4000) lines = 4000;
-            var baseAmount = lines / 10.0;
-            var thisAmount = baseAmount;
-            switch (play.Type)
-            {
-                case "tragedy":
-                    if (perf.Audience > 30)
-                    {
-                        thisAmount += 10 * (perf.Audience - 30);
-                    }
-                    break;
-                case "comedy":
-                    thisAmount += 3 * perf.Audience;
-                    if (perf.Audience > 20)
-                    {
-                        thisAmount += 100 + 5 * (perf.Audience - 20);
-                    }
-                    break;
-                case "history":
-                    var tragedyAmount = baseAmount;
-                    if (perf.Audience > 30)
-                    {
-                        tragedyAmount += 10 * (perf.Audience - 30);
-                    }
+            var result = new StringBuilder();
+            result.AppendFormat("Statement for {0}\n", invoice.Customer);
 
-                    var comedyAmount = baseAmount + (3 * perf.Audience);
-                    if (perf.Audience > 20)
-                    {
-                        comedyAmount += 100 + 5 * (perf.Audience - 20);
-                    }
+            var totalAmount = 0m;
+            var volumeCredits = 0;
 
-                    thisAmount = tragedyAmount + comedyAmount;
-                    break;
-                default:
-                    throw new Exception("unknown type: " + play.Type);
-            }
-            // add volume credits
-            volumeCredits += Math.Max(perf.Audience - 30, 0);
-            // add extra credit for every ten comedy/history attendees
-            if (play.Type == "comedy")
+            foreach (var perf in invoice.Performances)
             {
-                volumeCredits += (int)Math.Floor((decimal)perf.Audience / 5);
+                var play = plays[perf.PlayId];
+                var thisAmount = calculatorPrice.CalculateAmount(play, perf);
+                volumeCredits += calculatorPrice.CalculateVolumeCredits(play, perf);
+
+                result.AppendFormat(_cultureInfo, "  {0}: {1:C2} ({2} seats)\n", play.Name, thisAmount, perf.Audience);
+                totalAmount += thisAmount;
             }
-            // print line for this order
-            result += String.Format(cultureInfo, "  {0}: {1:C} ({2} seats)\n", play.Name, Convert.ToDecimal(thisAmount), perf.Audience);
-            totalAmount += (int)Math.Round(thisAmount * 100);
+
+            result.AppendFormat(_cultureInfo, "Amount owed is {0:C2}\n", totalAmount);
+            result.AppendFormat("You earned {0} credits\n", volumeCredits);
+
+            return result.ToString();
         }
-        result += String.Format(cultureInfo, "Amount owed is {0:C}\n", Convert.ToDecimal(totalAmount / 100.0));
-        result += String.Format("You earned {0} credits\n", volumeCredits);
-        return result;
     }
 }
