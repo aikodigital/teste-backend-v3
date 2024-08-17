e8375d961dc70ecaccf9fc000b532d387e49547c
Hugo Vidal <vidalshugo1@gmai.com>
2024-08-17 12:08:45 -0300

    Adicionado testes unitarios de tipo de genero e formato

--- TheatricalPlayersRefactoringKata.Tests/StatementPrinterTests.cs
+++ TheatricalPlayersRefactoringKata.Tests/StatementPrinterTests.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using ApprovalTests;
 using ApprovalTests.Reporters;
-using Microsoft.SqlServer.Server;
 using TheatricalPlayersRefactoringKata.Models;
 using TheatricalPlayersRefactoringKata.Services;
 using Xunit;
@@ -95,5 +94,73 @@ public class StatementPrinterTests
         var result = statementPrinter.Print(invoice, plays, "xml");
 
         Approvals.Verify(result);
+    }
+
+    [Fact]
+    public void TestText_NotFound_StatementExample()
+    {
+        var plays = new Dictionary<string, Play>();
+        plays.Add("hamlet", new Play("Hamlet", 4024, "tragedy"));
+        plays.Add("as-like", new Play("As You Like It", 2670, "comedy"));
+        plays.Add("as-like30", new Play("As You Like It:2030", 2671, "futuristic")); // genero nao suportado
+
+        Invoice invoice = new Invoice(
+            "BigCo",
+            new List<Performance>
+            {
+                new Performance("hamlet", 55),
+                new Performance("as-like", 35),
+                new Performance("as-like30", 34),
+            }
+        );
+
+        StatementPrinter statementPrinter = new StatementPrinter();
+        var exception = Assert.Throws<Exception>(() => statementPrinter.Print(invoice, plays, "text"));
+
+        Assert.Equal("unknown type: futuristic", exception.Message);
+    }
+    [Fact]
+    public void TestXml_NotFound_StatementExample()
+    {
+        var plays = new Dictionary<string, Play>();
+        plays.Add("hamlet", new Play("Hamlet", 4024, "tragedy"));
+        plays.Add("as-like", new Play("As You Like It", 2670, "comedy"));
+        plays.Add("as-like30", new Play("As You Like It:2030", 2671, "futuristic")); // genero nao suportado
+
+        Invoice invoice = new Invoice(
+            "BigCo",
+            new List<Performance>
+            {
+                new Performance("hamlet", 55),
+                new Performance("as-like", 35),
+                new Performance("as-like30", 34),
+            }
+        );
+
+        StatementPrinter statementPrinter = new StatementPrinter();
+        var exception = Assert.Throws<Exception>(() => statementPrinter.Print(invoice, plays, "xml"));
+
+        Assert.Equal("unknown type: futuristic", exception.Message);
+    }
+    [Fact]
+    public void TestFormat_NotSupported_StatementExample()
+    {
+        var plays = new Dictionary<string, Play>();
+        plays.Add("hamlet", new Play("Hamlet", 4024, "tragedy"));
+        plays.Add("as-like", new Play("As You Like It", 2670, "comedy"));
+
+        Invoice invoice = new Invoice(
+            "BigCo",
+            new List<Performance>
+            {
+                new Performance("hamlet", 55),
+                new Performance("as-like", 35),
+            }
+        );
+
+        StatementPrinter statementPrinter = new StatementPrinter();
+        var exception = Assert.Throws<ArgumentException>(() => statementPrinter.Print(invoice, plays, "html")); // Formato nao suportado
+
+        Assert.Equal("Unsupported format: html", exception.Message);
     }
 }
--- TheatricalPlayersRefactoringKata/Services/StatementPrinter.cs
+++ TheatricalPlayersRefactoringKata/Services/StatementPrinter.cs
@@ -1,11 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Xml.Linq;
-using TheatricalPlayersRefactoringKata.Calculators;
 using TheatricalPlayersRefactoringKata.Formatters;
 using TheatricalPlayersRefactoringKata.Models;
 
