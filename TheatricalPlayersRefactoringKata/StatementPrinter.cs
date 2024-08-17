96a8b5bc451d23d7813bbdf3b3cf51f5cdc9ecf0
LUCAS <luk4w@outlook.com>
2024-08-17 01:11:27 -0300

    refactor StatementPrinter to use new theatrical classes

--- TheatricalPlayersRefactoringKata/StatementPrinter.cs
+++ TheatricalPlayersRefactoringKata/StatementPrinter.cs
@@ -9,45 +9,32 @@ public class StatementPrinter
 {
     public string Print(Invoice invoice, Dictionary<string, Play> plays)
     {
-        var totalAmount = 0;
-        var volumeCredits = 0;
-        var result = string.Format("Statement for {0}\n", invoice.Customer);
+        int totalAmount = 0;
+        int volumeCredits = 0;
+        string result = string.Format("Statement for {0}\n", invoice.Customer);
         CultureInfo cultureInfo = new CultureInfo("en-US");
 
         foreach(var perf in invoice.Performances) 
         {
-            var play = plays[perf.PlayId];
-            var lines = play.Lines;
-            if (lines < 1000) lines = 1000;
-            if (lines > 4000) lines = 4000;
-            var thisAmount = lines * 10;
-            switch (play.Type) 
+            Play play = plays[perf.PlayId];
+            int lines = play.Lines;
+            Genre genre = play.Type switch
             {
-                case TheatricalType.Tragedy:
-                    if (perf.Audience > 30) {
-                        thisAmount += 1000 * (perf.Audience - 30);
-                    }
-                    break;
-                case TheatricalType.Comedy:
-                    if (perf.Audience > 20) {
-                        thisAmount += 10000 + 500 * (perf.Audience - 20);
-                    }
-                    thisAmount += 300 * perf.Audience;
-                    break;
-                default:
-                    throw new Exception("unknown type: " + play.Type);
-            }
-            // add volume credits
-            volumeCredits += Math.Max(perf.Audience - 30, 0);
-            // add extra credit for every ten comedy attendees
-            if (TheatricalType.Comedy == play.Type) volumeCredits += (int)Math.Floor((decimal)perf.Audience / 5);
-
+                TheatricalType.Tragedy => new Tragedy(),
+                TheatricalType.Comedy => new Comedy(),
+                TheatricalType.History => new History(),
+                _ => throw new Exception($"Unknown genre {play.Type}")
+            };
+            // calculate this amount
+            int thisAmount = genre.CalculateAmount(perf.Audience, lines);
+            // calculate volume credits
+            volumeCredits += genre.CalculateVolumeCredits(perf.Audience);
             // print line for this order
-            result += String.Format(cultureInfo, "  {0}: {1:C} ({2} seats)\n", play.Name, Convert.ToDecimal(thisAmount / 100), perf.Audience);
+            result += string.Format(cultureInfo, "  {0}: {1:C} ({2} seats)\n", play.Name, Convert.ToDecimal(thisAmount / 100m), perf.Audience);
             totalAmount += thisAmount;
         }
-        result += String.Format(cultureInfo, "Amount owed is {0:C}\n", Convert.ToDecimal(totalAmount / 100));
-        result += String.Format("You earned {0} credits\n", volumeCredits);
+        result += string.Format(cultureInfo, "Amount owed is {0:C}\n", Convert.ToDecimal(totalAmount / 100m));
+        result += string.Format("You earned {0} credits\n", volumeCredits);
         return result;
     }
 }
