50871d9d47ca8b75dac79a6b3d7e036c88e1ce0d
LUCAS <luk4w@outlook.com>
2024-08-17 16:53:36 -0300

    add XML output format to StatementPrinter

--- TheatricalPlayersRefactoringKata.Tests/StatementPrinterTests.cs
+++ TheatricalPlayersRefactoringKata.Tests/StatementPrinterTests.cs
@@ -28,8 +28,8 @@ public class StatementPrinterTests
             }
         );
 
-        StatementPrinter statementPrinter = new StatementPrinter();
-        var result = statementPrinter.Print(invoice, plays);
+        StatementPrinter statementPrinter = new StatementPrinter(invoice, plays);
+        var result = statementPrinter.TxtPrint();
 
         Approvals.Verify(result);
     }
@@ -59,8 +59,39 @@ public class StatementPrinterTests
             }
         );
 
-        StatementPrinter statementPrinter = new StatementPrinter();
-        var result = statementPrinter.Print(invoice, plays);
+        StatementPrinter statementPrinter = new StatementPrinter(invoice, plays);
+        var result = statementPrinter.TxtPrint();
+
+        Approvals.Verify(result);
+    }
+
+    [Fact]
+    [UseReporter(typeof(DiffReporter))]
+    public void TestXmlStatementExample()
+    {
+        var plays = new Dictionary<string, Play>();
+        plays.Add("hamlet", new Play("Hamlet", 4024, TheatricalType.Tragedy));
+        plays.Add("as-like", new Play("As You Like It", 2670, TheatricalType.Comedy));
+        plays.Add("othello", new Play("Othello", 3560, TheatricalType.Tragedy));
+        plays.Add("henry-v", new Play("Henry V", 3227, TheatricalType.History));
+        plays.Add("john", new Play("King John", 2648, TheatricalType.History));
+        plays.Add("richard-iii", new Play("Richard III", 3718, TheatricalType.History));
+
+        Invoice invoice = new Invoice(
+            "BigCo",
+            new List<Performance>
+            {
+                new Performance("hamlet", 55),
+                new Performance("as-like", 35),
+                new Performance("othello", 40),
+                new Performance("henry-v", 20),
+                new Performance("john", 39),
+                new Performance("henry-v", 20)
+            }
+        );
+
+        StatementPrinter statementPrinter = new StatementPrinter(invoice, plays);
+        var result = statementPrinter.XmlPrint();
 
         Approvals.Verify(result);
     }
--- TheatricalPlayersRefactoringKata/StatementPrinter.cs
+++ TheatricalPlayersRefactoringKata/StatementPrinter.cs
@@ -1,41 +1,138 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.IO;
+using System.Text;
+using System.Xml;
 using TheatricalPlayersRefactoringKata.Enums;
 using TheatricalPlayersRefactoringKata.TheatricalGenre;
 
-namespace TheatricalPlayersRefactoringKata;
-
-public class StatementPrinter
+namespace TheatricalPlayersRefactoringKata
 {
-    public string Print(Invoice invoice, Dictionary<string, Play> plays)
+    public class StatementLine
     {
-        int totalAmount = 0;
-        int volumeCredits = 0;
-        string result = string.Format("Statement for {0}\n", invoice.Customer);
-        CultureInfo cultureInfo = new CultureInfo("en-US");
-
-        foreach(var perf in invoice.Performances) 
+        public string Name { get; set; }
+        public decimal Amount { get; set; }
+        public int Seats { get; set; }
+        public int Credits { get; set; }
+        /// <summary>
+        /// Represents a line in a statement
+        /// </summary>
+        /// <param name="name">Name of the play</param>
+        /// <param name="amount">Amount of the play</param>
+        /// <param name="seats">Number of occupied seats</param>
+        public StatementLine(string name, decimal amount, int credits, int seats)
         {
-            Play play = plays[perf.PlayId];
-            int lines = play.Lines;
-            Genre genre = play.Type switch
-            {
-                TheatricalType.Tragedy => new Tragedy(),
-                TheatricalType.Comedy => new Comedy(),
-                TheatricalType.History => new History(),
-                _ => throw new Exception($"Unknown theatrical genre {play.Type}")
-            };
-            // calculate this amount
-            int thisAmount = genre.CalculateAmount(perf.Audience, lines);
-            // calculate volume credits
-            volumeCredits += genre.CalculateVolumeCredits(perf.Audience);
-            // print line for this order
-            result += string.Format(cultureInfo, "  {0}: {1:C} ({2} seats)\n", play.Name, Convert.ToDecimal(thisAmount / 100m), perf.Audience);
-            totalAmount += thisAmount;
+            Name = name;
+            Amount = amount;
+            Seats = seats;
+            Credits = credits;
         }
-        result += string.Format(cultureInfo, "Amount owed is {0:C}\n", Convert.ToDecimal(totalAmount / 100m));
-        result += string.Format("You earned {0} credits\n", volumeCredits);
-        return result;
+    }
+
+    public class StatementPrinter
+    {
+        private readonly string _costumer;
+        private readonly decimal _totalAmount;
+        private readonly int _volumeCredits;
+        private List<StatementLine> _lines;
+
+        /// <summary>
+        /// Represents a statement printer for a costumer
+        /// </summary>
+        /// <param name="invoice">Invoice of the costumer</param>
+        /// <param name="plays">Plays of the costumer</param>
+        public StatementPrinter(Invoice invoice, Dictionary<string, Play> plays)
+        {
+            _costumer = invoice.Customer;
+            _lines = new List<StatementLine>();
+            _totalAmount = 0;
+            _volumeCredits = 0;
+            foreach (var perf in invoice.Performances)
+            {
+                Play play = plays[perf.PlayId];
+                int lines = play.Lines;
+                Genre genre = play.Type switch
+                {
+                    TheatricalType.Tragedy => new Tragedy(),
+                    TheatricalType.Comedy => new Comedy(),
+                    TheatricalType.History => new History(),
+                    _ => throw new Exception($"Unknown theatrical genre {play.Type}")
+                };
+                // calculate this amount in dollar
+                decimal thisAmount = Convert.ToDecimal(genre.CalculateAmount(perf.Audience, lines) / 100m);
+                // calculate volume credits
+                int credits = genre.CalculateVolumeCredits(perf.Audience);
+                // save line for this order
+                _lines.Add(new StatementLine(play.Name, thisAmount, credits, perf.Audience));
+                // increase total amount and credits
+                _volumeCredits += credits;
+                _totalAmount += thisAmount;
+            }
+        }
+
+        /// <summary>
+        /// Print the statement in text format
+        /// </summary>
+        /// <returns> Text string </returns>
+        public string TxtPrint()
+        {
+            var result = new StringBuilder();
+            CultureInfo cultureInfo = new CultureInfo("en-US");
+            result.AppendFormat("Statement for {0}\n", _costumer);
+            foreach (StatementLine line in _lines)
+            {
+                result.AppendFormat(cultureInfo, "  {0}: {1:C} ({2} seats)\n", line.Name, line.Amount, line.Seats);
+            }
+            result.AppendFormat(cultureInfo, "Amount owed is {0:C}\n", _totalAmount);
+            result.AppendFormat("You earned {0} credits\n", _volumeCredits);
+            return result.ToString();
+        }
+
+        /// <summary>
+        /// Print the statement in XML format
+        /// </summary>
+        /// <returns> XML string </returns>
+        public string XmlPrint()
+        {
+            XmlWriterSettings settings = new XmlWriterSettings
+            {
+                Indent = true,
+                Encoding = Encoding.UTF8
+            };
+            // fix UTF-8 encoding
+            using var memoryStream = new MemoryStream();
+            using (XmlWriter writer = XmlWriter.Create(memoryStream, settings))
+            {
+                // create root element
+                writer.WriteStartElement("Statement");
+                // add xmlns:xsi
+                writer.WriteAttributeString("xmlns", "xsi", null, "http://www.w3.org/2001/XMLSchema-instance");
+                // add xmlns:xsd
+                writer.WriteAttributeString("xmlns", "xsd", null, "http://www.w3.org/2001/XMLSchema");
+                // add customer element
+                writer.WriteElementString("Customer", _costumer);
+                // add elements for each line
+                writer.WriteStartElement("Items");
+                foreach (var line in _lines)
+                {
+                    writer.WriteStartElement("Item");
+                    writer.WriteElementString("AmountOwed", line.Amount.ToString(CultureInfo.InvariantCulture));
+                    writer.WriteElementString("EarnedCredits", line.Credits.ToString());
+                    writer.WriteElementString("Seats", line.Seats.ToString());
+                    writer.WriteEndElement();
+                }
+                writer.WriteEndElement();
+                // add AmountOwed element
+                writer.WriteElementString("AmountOwed", _totalAmount.ToString(CultureInfo.InvariantCulture));
+                // add EarnedCredits element
+                writer.WriteElementString("EarnedCredits", _volumeCredits.ToString());
+                // close
+                writer.WriteEndElement();
+                writer.WriteEndDocument();
+            }
+            // Convert the MemoryStream to a UTF-8 encoded string
+            return Encoding.UTF8.GetString(memoryStream.ToArray());
+        }
     }
 }
