3f474473f00b1604d26e6aec508239937c3aa103
LUCAS <luk4w@outlook.com>
2024-08-17 00:45:40 -0300

    add Genre base class and subclasses

---
+++ TheatricalPlayersRefactoringKata/Comedy.cs
@@ -0,0 +1,18 @@
+namespace TheatricalPlayersRefactoringKata
+{
+    public class Comedy : Genre
+    {
+        public override int CalculateAmount(int audience, int lines)
+        {
+            int amount = base.CalculateAmount(audience, lines);
+            if (audience > 20) amount += 10000 + 500 * (audience - 20);
+            amount += 300 * audience;
+            return amount;
+        }
+
+        public override int CalculateVolumeCredits(int audience)
+        {
+            return base.CalculateVolumeCredits(audience) + (int)System.Math.Floor((decimal)audience / 5);
+        }
+    }
+}
---
+++ TheatricalPlayersRefactoringKata/Genre.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace TheatricalPlayersRefactoringKata
+{
+    public abstract class Genre
+    {
+        /// <summary>
+        /// Calculates the amount for a performance based on the number of lines.
+        /// </summary>
+        /// <param name="audience">The number of audience members.</param>
+        /// <param name="lines">The number of lines in the performance.</param>
+        /// <returns>The amount in cents.</returns>
+        public virtual int CalculateAmount(int audience, int lines)
+        {
+            if (lines < 1000) lines = 1000;
+            if (lines > 4000) lines = 4000;
+            return lines * 10;
+        }
+
+        /// <summary>
+        /// Calculates volume credits based on the audience size.
+        /// </summary>
+        /// <param name="audience">The number of audience members attending the performance.</param>
+        /// <returns>The volume credits earned.</returns>
+        public virtual int CalculateVolumeCredits(int audience)
+        {
+            return Math.Max(audience - 30, 0);
+        }
+    }
+}
