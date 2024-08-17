8ee084709bc3e64295e80d41678fc32bd4c5f350
rGabriel <126360827+rGabrielLima@users.noreply.github.com>
2024-08-16 23:39:04 -0300

    Primeiro push, falta compreender como fazer algumas alterações

---
+++ TheatricalPlayersRefactoringKata/ChargeTragedy.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using static TheatricalPlayersRefactoringKata.StatementPrinter;
+
+namespace TheatricalPlayersRefactoringKata
+{   
+    //???
+    public class CobrancaTragedia : IChargeStrategy
+    {
+
+        public int CalcularCobranca(Performance performance, Play play)
+        {
+            // limites de linhas
+            var lines = play.Lines;
+            if (lines < 1000) lines = 1000;
+            if (lines > 4000) lines = 4000;
+
+            // a base da cobrança são as linhas x10
+            var amount = lines * 10;
+
+            // adiciona valor se a audiência for superior a 30
+            if (performance.Audience > 30)
+            {
+                amount += 1000 * (performance.Audience - 30);
+            }
+
+            return amount;
+        }
+
+        public int CalcularCreditos(Performance performance)
+        {
+            // crédito para audiência maior que 30
+            return Math.Max(performance.Audience - 30, 0);
+        }
+
+    }
+}
--- TheatricalPlayersRefactoringKata/StatementPrinter.cs
+++ TheatricalPlayersRefactoringKata/StatementPrinter.cs
@@ -1,17 +1,24 @@
-﻿using System;
+﻿using MySqlX.XDevAPI.Common;
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 
 namespace TheatricalPlayersRefactoringKata;
 
 public class StatementPrinter
-{
+{   
+    public interface IChargeStrategy
+    {
+        int CalculateBilling(Performance performance, Play play);
+        int CalculateCredits(Performance performance);
+    }
+
     public string Print(Invoice invoice, Dictionary<string, Play> plays)
     {
         var totalAmount = 0;
         var volumeCredits = 0;
         var result = string.Format("Statement for {0}\n", invoice.Customer);
-        CultureInfo cultureInfo = new CultureInfo("en-US");
+        CultureInfo cultureInfo = new("en-US");
 
         foreach(var perf in invoice.Performances) 
         {
@@ -48,5 +55,10 @@ public class StatementPrinter
         result += String.Format(cultureInfo, "Amount owed is {0:C}\n", Convert.ToDecimal(totalAmount / 100));
         result += String.Format("You earned {0} credits\n", volumeCredits);
         return result;
+
     }
+    
+
+    
 }
+
