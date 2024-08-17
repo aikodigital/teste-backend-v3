8e833f9e16e2c13106129f80ab8b2e4599af9112
Hugo Vidal <vidalshugo1@gmai.com>
2024-08-17 14:38:49 -0300

    Adicionado Api Rest e conexão inicial com banco de dados

---
+++ ApiRest/infra/DbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace TheatricalPlayersRefactoringKata.infra
+{
+    internal class ApiDbContext : DbContext
+    {
+        private IConfiguration _configuration;
+
+        public ApiDbContext(IConfiguration configuration, DbContextOptions options) : base(options)
+        {
+                _configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var typeDataBase = _configuration["TypeDatabase"];
+            var connectionString = _configuration.GetConnectionString(typeDataBase);
+
+            optionsBuilder.UseNpgsql(connectionString);
+        }
+    }
+}
