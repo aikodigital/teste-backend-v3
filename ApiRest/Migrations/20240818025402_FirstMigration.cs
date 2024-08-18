f3ccdacbf131c62367a85a00327bcb5fb49eb57b
Hugo Vidal <vidalshugo1@gmai.com>
2024-08-17 23:54:41 -0300

    Adicionado Migrations corrigidos

---
+++ ApiRest/Migrations/20240818025402_FirstMigration.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace TheatricalPlayersRefactoringKata.API.Migrations
+{
+    public partial class FirstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Invoices",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Customer = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Invoices", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Plays",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "text", nullable: false),
+                    Name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Type = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Lines = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Plays", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Performances",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    PlayName = table.Column<string>(type: "text", nullable: true),
+                    Audience = table.Column<int>(type: "integer", nullable: false),
+                    InvoiceId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Performances", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Performance_PlayName",
+                        column: x => x.PlayName,
+                        principalTable: "Plays",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Performances_Invoices_InvoiceId",
+                        column: x => x.InvoiceId,
+                        principalTable: "Invoices",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Performances_InvoiceId",
+                table: "Performances",
+                column: "InvoiceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Performances_PlayName",
+                table: "Performances",
+                column: "PlayName");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Plays_Name",
+                table: "Plays",
+                column: "Name",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Performances");
+
+            migrationBuilder.DropTable(
+                name: "Plays");
+
+            migrationBuilder.DropTable(
+                name: "Invoices");
+        }
+    }
+}
