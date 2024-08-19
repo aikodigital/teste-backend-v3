ec15dfdf9e0ab7bc9bfd19d2b700d29b404ea711
Bruno0M <brunomedeirosr09@gmail.com>
2024-08-18 21:14:50 -0300

    feat: add formatter for xml - add Infrastructure layer - add string writer for utf8

---
+++ TheatricalPlayersRefactoringKata.Application/Services/XmlStatementFormatter.cs
@@ -0,0 +1,46 @@
+﻿using System.Xml.Linq;
+using TheatricalPlayersRefactoringKata.Application.Interfaces;
+using TheatricalPlayersRefactoringKata.Domain.Entities;
+using TheatricalPlayersRefactoringKata.Infrastructure.Utils;
+
+namespace TheatricalPlayersRefactoringKata.Application.Services
+{
+    public class XmlStatementFormatter : IStatementFormatter
+    {
+        public string Format(Invoice invoice, Dictionary<string, Play> plays, Dictionary<Performance, int> performanceAmounts, int volumeCredits, decimal totalAmount)
+        {
+            List<XElement> items = new() { };
+
+            foreach (var perf in invoice.Performances)
+            {
+                var play = plays[perf.PlayId];
+                decimal baseAmount = performanceAmounts[perf];
+
+                var element = new XElement("Item",
+                    new XElement("AmountOwed", Convert.ToDecimal(baseAmount / 100)),
+                    new XElement("EarnedCredits", perf.Credits),
+                    new XElement("Seats", perf.Audience)
+                );
+
+                items.Add(element);
+            }
+
+            var xml = new XDocument(
+                new XDeclaration("1.0", "utf-8", null),
+                new XElement("Statement",
+                    new XAttribute(XNamespace.Xmlns + "xsi", "http://www.w3.org/2001/XMLSchema-instance"),
+                    new XAttribute(XNamespace.Xmlns + "xsd", "http://www.w3.org/2001/XMLSchema"),
+                    new XElement("Customer", invoice.Customer),
+                    new XElement("Items", items),
+                    new XElement("AmountOwed", Convert.ToDecimal(totalAmount / 100)),
+                    new XElement("EarnedCredits", volumeCredits)
+                )
+            );
+
+            using var wr = new Utf8StringWriter();
+
+            xml.Save(wr);
+            return wr.ToString();
+        }
+    }
+}
---
+++ TheatricalPlayersRefactoringKata.Infrastructure/Utils/Utf8StringWriter.cs
@@ -0,0 +1,9 @@
+﻿using System.Text;
+
+namespace TheatricalPlayersRefactoringKata.Infrastructure.Utils
+{
+    public class Utf8StringWriter : StringWriter
+    {
+        public override Encoding Encoding => Encoding.UTF8;
+    }
+}
