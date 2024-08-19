f48b68bf0e995582e90aa6c67aa529618e6dada0
Bruno0M <brunomedeirosr09@gmail.com>
2024-08-19 06:40:34 -0300

    feat: add DbContext - add package 'Microsoft.EntityFrameworkCore'

---
+++ TheatricalPlayersRefactoringKata.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TheatricalPlayersRefactoringKata.Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
