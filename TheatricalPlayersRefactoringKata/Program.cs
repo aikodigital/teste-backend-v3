a087732d427ab196913ce3f9577d0d6244f310f2
Caslu-Parra <lucasparra2@outlook.com>
2024-08-19 12:16:15 -0300

    Estrutura de diretórios em formato MVC

---
+++ TheatricalPlayersRefactoringKata/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Configuração Swagger/OpenAPI
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Aiko Theatre API",
+        Version = "v1",
+        Description = "Atividade de refatoração de código proposta pela Aiko.",
+    });
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Aiko Theatre API");
+        c.RoutePrefix = string.Empty;
+    });
+}
+
+app.MapGet("", () => Results.Ok("Saudações, caro espectador!"));
+
+app.Run();
