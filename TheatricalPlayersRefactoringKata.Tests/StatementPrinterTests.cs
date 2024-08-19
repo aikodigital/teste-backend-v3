8be8f534716eafbb202c9d992b737c0757ecc942
Bruno0M <brunomedeirosr09@gmail.com>
2024-08-18 21:29:43 -0300

    test: add 'TestXmlStatementExample' - adjust the BOM of the first line of 'approved.txt'

--- TheatricalPlayersRefactoringKata.Tests/StatementPrinterTests.cs
+++ TheatricalPlayersRefactoringKata.Tests/StatementPrinterTests.cs
@@ -41,13 +41,15 @@ public class StatementPrinterTests
     [UseReporter(typeof(DiffReporter))]
     public void TestTextStatementExample()
     {
-        var plays = new Dictionary<string, Play>();
-        plays.Add("hamlet", new Play("Hamlet", 4024, Genre.Tragedy));
-        plays.Add("as-like", new Play("As You Like It", 2670, Genre.Comedy));
-        plays.Add("othello", new Play("Othello", 3560, Genre.Tragedy));
-        plays.Add("henry-v", new Play("Henry V", 3227, Genre.History));
-        plays.Add("john", new Play("King John", 2648, Genre.History));
-        plays.Add("richard-iii", new Play("Richard III", 3718, Genre.History));
+        var plays = new Dictionary<string, Play>
+        {
+            { "hamlet", new Play("Hamlet", 4024, Genre.Tragedy) },
+            { "as-like", new Play("As You Like It", 2670, Genre.Comedy) },
+            { "othello", new Play("Othello", 3560, Genre.Tragedy) },
+            { "henry-v", new Play("Henry V", 3227, Genre.History) },
+            { "john", new Play("King John", 2648, Genre.History) },
+            { "richard-iii", new Play("Richard III", 3718, Genre.History) }
+        };
 
         Invoice invoice = new Invoice(
             "BigCo",
@@ -63,6 +65,40 @@ public class StatementPrinterTests
         );
 
         var statementFormatter = new TextStatementFormatter();
+        var statementPrinter = new StatementPrinter(statementFormatter);
+        var result = statementPrinter.Print(invoice, plays);
+
+        Approvals.Verify(result);
+    }
+
+    [Fact]
+    [UseReporter(typeof(DiffReporter))]
+    public void TestXmlStatementExample()
+    {
+        var plays = new Dictionary<string, Play>
+        {
+            { "hamlet", new Play("Hamlet", 4024, Genre.Tragedy) },
+            { "as-like", new Play("As You Like It", 2670, Genre.Comedy) },
+            { "othello", new Play("Othello", 3560, Genre.Tragedy) },
+            { "henry-v", new Play("Henry V", 3227, Genre.History) },
+            { "john", new Play("King John", 2648, Genre.History) },
+            { "richard-iii", new Play("Richard III", 3718, Genre.History) }
+        };
+
+        var invoice = new Invoice(
+            "BigCo",
+            new List<Performance>
+            {
+                new Performance("hamlet", 55),
+                new Performance("as-like", 35),
+                new Performance("othello", 40),
+                new Performance("henry-v", 20),
+                new Performance("john", 39),
+                new Performance("henry-v", 20)
+            }
+        );
+
+        var statementFormatter = new XmlStatementFormatter();
         var statementPrinter = new StatementPrinter(statementFormatter);
         var result = statementPrinter.Print(invoice, plays);
 
