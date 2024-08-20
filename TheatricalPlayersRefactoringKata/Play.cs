c953e01e9a845e4523103759de1d5b77e4c15c89
Possari <pedropossari2@gmail.com>
2024-08-20 18:37:16 -0300

    Atualização classe play
    Estou colocando os cálculos básicos das novas classes que serão implementadas.
    
    Tendo a classe play como pai

--- TheatricalPlayersRefactoringKata/Play.cs
+++ TheatricalPlayersRefactoringKata/Play.cs
@@ -1,18 +1,38 @@
+using System;
+
 namespace TheatricalPlayersRefactoringKata;
 
 public class Play
 {
-    private string _name;
-    private int _lines;
-    private string _type;
+    public string Name { get; set; }
+    public int NumberLines { get; set; }
+    public int Audience { get; set; }
 
-    public string Name { get => _name; set => _name = value; }
-    public int Lines { get => _lines; set => _lines = value; }
-    public string Type { get => _type; set => _type = value; }
+    protected const int MinLines = 1000;
+    protected const int MaxLines = 4000;
 
-    public Play(string name, int lines, string type) {
-        this._name = name;
-        this._lines = lines;
-        this._type = type;
+    protected virtual double CalculatorValorBase()
+    {
+        throw new NotImplementedException("This method must be implemented in a derived class.");
+    }
+
+    public virtual double CalculatorValor()
+    {
+        return CalculatorValorBase();
+    }
+
+    public virtual int CalculatorCredits()
+    {
+        if (Audience <= 30)
+        {
+            return 0;
+        }
+        return Math.Max(Audience - 30, 0);
+    }
+
+    public Play(string name, int lines)
+    {
+        this.Name = name;
+        this.NumberLines = lines;
     }
 }
