bd84bab4a98f98eec06ab77069a97a7630f7d06a
Marcos <m.vinicius.sr11@gmail.com>
2024-08-20 15:37:39 -0300

    mais casos sendo testados em PerformancePriceTests

--- TheatricalPlayersRefactoringKata.Tests/PerformancePriceTests.cs
+++ TheatricalPlayersRefactoringKata.Tests/PerformancePriceTests.cs
@@ -17,24 +17,96 @@ namespace TheatricalPlayersRefactoringKata.Tests
         [UseReporter(typeof(DiffReporter))]
         public void TragedyUseCaseTest()
         {
-            var thisAmount = PerformancePrice.Price(900, 21, PlayType.Tragedy);
-            Approvals.Verify(thisAmount);
+            var amount01 = PerformancePrice.Price(900, 15, PlayType.Tragedy);
+            var amount02 = PerformancePrice.Price(900, 20, PlayType.Tragedy);
+            var amount03 = PerformancePrice.Price(900, 25, PlayType.Tragedy);
+            var amount04 = PerformancePrice.Price(900, 30, PlayType.Tragedy);
+            var amount05 = PerformancePrice.Price(900, 35, PlayType.Tragedy);
+
+            var amount06 = PerformancePrice.Price(2000, 15, PlayType.Tragedy);
+            var amount07 = PerformancePrice.Price(2000, 20, PlayType.Tragedy);
+            var amount08 = PerformancePrice.Price(2000, 25, PlayType.Tragedy);
+            var amount09 = PerformancePrice.Price(2000, 30, PlayType.Tragedy);
+            var amount10 = PerformancePrice.Price(2000, 35, PlayType.Tragedy);
+
+            var amount11 = PerformancePrice.Price(4500, 15, PlayType.Tragedy);
+            var amount12 = PerformancePrice.Price(4500, 20, PlayType.Tragedy);
+            var amount13 = PerformancePrice.Price(4500, 25, PlayType.Tragedy);
+            var amount14 = PerformancePrice.Price(4500, 30, PlayType.Tragedy);
+            var amount15 = PerformancePrice.Price(4500, 35, PlayType.Tragedy);
+
+            string result = amount01.ToString() + " " + amount02.ToString() + " " +
+                amount03.ToString() + " " + amount04.ToString() + " " + amount05.ToString() + "\n" +
+                amount06.ToString() + " " + amount07.ToString() + " " +
+                amount08.ToString() + " " + amount09.ToString() + " " + amount10.ToString() + "\n" +
+                amount11.ToString() + " " + amount12.ToString() + " " +
+                amount13.ToString() + " " + amount14.ToString() + " " + amount15.ToString();
+
+            Approvals.Verify(result);
         }
 
         [Fact]
         [UseReporter(typeof(DiffReporter))]
         public void ComedyUseCaseTest()
         {
-            var thisAmount = PerformancePrice.Price(900, 21, PlayType.Comedy);
-            Approvals.Verify(thisAmount);
+            var amount01 = PerformancePrice.Price(900, 15, PlayType.Comedy);
+            var amount02 = PerformancePrice.Price(900, 20, PlayType.Comedy);
+            var amount03 = PerformancePrice.Price(900, 25, PlayType.Comedy);
+            var amount04 = PerformancePrice.Price(900, 30, PlayType.Comedy);
+            var amount05 = PerformancePrice.Price(900, 35, PlayType.Comedy);
+
+            var amount06 = PerformancePrice.Price(2000, 15, PlayType.Comedy);
+            var amount07 = PerformancePrice.Price(2000, 20, PlayType.Comedy);
+            var amount08 = PerformancePrice.Price(2000, 25, PlayType.Comedy);
+            var amount09 = PerformancePrice.Price(2000, 30, PlayType.Comedy);
+            var amount10 = PerformancePrice.Price(2000, 35, PlayType.Comedy);
+
+            var amount11 = PerformancePrice.Price(4500, 15, PlayType.Comedy);
+            var amount12 = PerformancePrice.Price(4500, 20, PlayType.Comedy);
+            var amount13 = PerformancePrice.Price(4500, 25, PlayType.Comedy);
+            var amount14 = PerformancePrice.Price(4500, 30, PlayType.Comedy);
+            var amount15 = PerformancePrice.Price(4500, 35, PlayType.Comedy);
+
+            string result = amount01.ToString() + " " + amount02.ToString() + " " +
+                amount03.ToString() + " " + amount04.ToString() + " " + amount05.ToString() + "\n" +
+                amount06.ToString() + " " + amount07.ToString() + " " +
+                amount08.ToString() + " " + amount09.ToString() + " " + amount10.ToString() + "\n" +
+                amount11.ToString() + " " + amount12.ToString() + " " +
+                amount13.ToString() + " " + amount14.ToString() + " " + amount15.ToString();
+
+            Approvals.Verify(result);
         }
 
         [Fact]
         [UseReporter(typeof(DiffReporter))]
         public void HistoryUseCaseTest()
         {
-            var thisAmount = PerformancePrice.Price(900, 21, PlayType.History);
-            Approvals.Verify(thisAmount);
+            var amount01 = PerformancePrice.Price(900, 15, PlayType.History);
+            var amount02 = PerformancePrice.Price(900, 20, PlayType.History);
+            var amount03 = PerformancePrice.Price(900, 25, PlayType.History);
+            var amount04 = PerformancePrice.Price(900, 30, PlayType.History);
+            var amount05 = PerformancePrice.Price(900, 35, PlayType.History);
+
+            var amount06 = PerformancePrice.Price(2000, 15, PlayType.History);
+            var amount07 = PerformancePrice.Price(2000, 20, PlayType.History);
+            var amount08 = PerformancePrice.Price(2000, 25, PlayType.History);
+            var amount09 = PerformancePrice.Price(2000, 30, PlayType.History);
+            var amount10 = PerformancePrice.Price(2000, 35, PlayType.History);
+
+            var amount11 = PerformancePrice.Price(4500, 15, PlayType.History);
+            var amount12 = PerformancePrice.Price(4500, 20, PlayType.History);
+            var amount13 = PerformancePrice.Price(4500, 25, PlayType.History);
+            var amount14 = PerformancePrice.Price(4500, 30, PlayType.History);
+            var amount15 = PerformancePrice.Price(4500, 35, PlayType.History);
+
+            string result = amount01.ToString() + " " + amount02.ToString() + " " +
+                amount03.ToString() + " " + amount04.ToString() + " " + amount05.ToString() + "\n" +
+                amount06.ToString() + " " + amount07.ToString() + " " +
+                amount08.ToString() + " " + amount09.ToString() + " " + amount10.ToString() + "\n" +
+                amount11.ToString() + " " + amount12.ToString() + " " +
+                amount13.ToString() + " " + amount14.ToString() + " " + amount15.ToString();
+
+            Approvals.Verify(result);
         }
     }
 }
