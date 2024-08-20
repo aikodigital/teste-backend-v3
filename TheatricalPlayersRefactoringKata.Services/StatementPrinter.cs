37299eb82531b6748c27895bd346b45d28643dd5
Marcos <m.vinicius.sr11@gmail.com>
2024-08-20 12:08:08 -0300

    implementado o método StatementPrinter.PrintXml

--- TheatricalPlayersRefactoringKata.Services/StatementPrinter.cs
+++ TheatricalPlayersRefactoringKata.Services/StatementPrinter.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Xml;
+using System.Xml.Linq;
 using TheatricalPlayersRefactoringKata.Enums;
 using TheatricalPlayersRefactoringKata.Models;
 using TheatricalPlayersRefactoringKata.Services.InvoicePrice;
@@ -9,7 +11,7 @@ namespace TheatricalPlayersRefactoringKata.Services;
 
 public class StatementPrinter
 {
-    public string Print(Invoice invoice)
+    public string PrintText(Invoice invoice)
     {
         var totalAmount = 0;
         var volumeCredits = 0;
@@ -18,18 +20,85 @@ public class StatementPrinter
 
         foreach(var perf in invoice.Performances) 
         {
-            var play = perf.Play;
             var thisAmount = PerformancePrice.Price(perf.Play.Lines, perf.Audience, perf.Play.Type);
 
             // add volume credits
             volumeCredits += PerformancePrice.Credits(perf.Audience, perf.Play.Type == PlayType.Comedy);
 
             // print line for this order
-            result += String.Format(cultureInfo, "  {0}: {1:C} ({2} seats)\n", play.Name, Convert.ToDecimal(((double)thisAmount) / 100), perf.Audience);
+            result += String.Format(cultureInfo, "  {0}: {1:C} ({2} seats)\n", perf.Play.Name, Convert.ToDecimal(((double)thisAmount) / 100), perf.Audience);
             totalAmount += thisAmount;
         }
         result += String.Format(cultureInfo, "Amount owed is {0:C}\n", Convert.ToDecimal(((double)totalAmount) / 100));
         result += String.Format("You earned {0} credits\n", volumeCredits);
         return result;
     }
+
+    public string PrintXml(Invoice invoice)
+    {
+        var totalAmount = 0;
+        var volumeCredits = 0;
+        CultureInfo cultureInfo = new CultureInfo("en-US");
+
+        var document = new XmlDocument();
+        //document.AppendChild(document.CreateXmlDeclaration("1.0", "utf-8", ""));
+
+        var statement = document.CreateElement("Statement");
+        statement.SetAttribute("xmlns:xsi", "http://www.w3.org/2001/XMLSchema-instance");
+        statement.SetAttribute("xmlns:xsd", "http://www.w3.org/2001/XMLSchema");
+        document.AppendChild(statement);
+
+        var customer = document.CreateElement("Customer");
+        customer.InnerText = invoice.Customer;
+        statement.AppendChild(customer);  
+
+        var items = document.CreateElement("Items");
+        statement.AppendChild(items);  
+
+        foreach (var perf in invoice.Performances)
+        {
+            var thisAmount = PerformancePrice.Price(perf.Play.Lines, perf.Audience, perf.Play.Type);
+            var currentCredits = PerformancePrice.Credits(perf.Audience, perf.Play.Type == PlayType.Comedy);
+            volumeCredits += currentCredits;
+
+            var item = document.CreateElement("Item");
+
+            var amount = document.CreateElement("AmountOwed");
+            var thisAmountDecimal = Convert.ToDecimal(((double)thisAmount) / 100);
+            amount.InnerText = thisAmountDecimal.ToString(cultureInfo);
+            item.AppendChild(amount);
+
+            var credits = document.CreateElement("EarnedCredits");
+            credits.InnerText = currentCredits.ToString();
+            item.AppendChild(credits);
+
+            var seats = document.CreateElement("Seats");
+            seats.InnerText = perf.Audience.ToString();
+            item.AppendChild(seats);
+
+            items.AppendChild(item);
+
+            totalAmount += thisAmount;
+        }
+
+        var amountOwed = document.CreateElement("AmountOwed");
+        var totalAmountDecimal = Convert.ToDecimal(((double)totalAmount) / 100);
+        amountOwed.InnerText = totalAmountDecimal.ToString(cultureInfo);
+        statement.AppendChild(amountOwed);  
+
+        var earnedCredits = document.CreateElement("EarnedCredits");
+        earnedCredits.InnerText = volumeCredits.ToString();
+        statement.AppendChild(earnedCredits);  
+        
+        using(var reader = new XmlNodeReader(document))
+        {
+            var result = XDocument.Load(reader, LoadOptions.PreserveWhitespace);
+
+            result.Declaration = new XDeclaration("1.0", "utf-8", null);
+
+            return "\uFEFF" + result.Declaration + Environment.NewLine + result;
+        }
+    }
+
+
 }
--- TheatricalPlayersRefactoringKata.Tests/StatementPrinterTests.cs
+++ TheatricalPlayersRefactoringKata.Tests/StatementPrinterTests.cs
@@ -31,7 +31,7 @@ public class StatementPrinterTests
         );
 
         StatementPrinter statementPrinter = new StatementPrinter();
-        var result = statementPrinter.Print(invoice);
+        var result = statementPrinter.PrintText(invoice);
 
         Approvals.Verify(result);
     }
@@ -62,7 +62,38 @@ public class StatementPrinterTests
         );
 
         StatementPrinter statementPrinter = new StatementPrinter();
-        var result = statementPrinter.Print(invoice);
+        var result = statementPrinter.PrintText(invoice);
+
+        Approvals.Verify(result);
+    }
+
+    [Fact]
+    [UseReporter(typeof(DiffReporter))]
+    public void TestXmlStatementExemple()
+    {
+        var plays = new Dictionary<string, Play>();
+        plays.Add("hamlet", new Play("Hamlet", 4024, PlayType.Tragedy));
+        plays.Add("as-like", new Play("As You Like It", 2670, PlayType.Comedy));
+        plays.Add("othello", new Play("Othello", 3560, PlayType.Tragedy));
+        plays.Add("henry-v", new Play("Henry V", 3227, PlayType.History));
+        plays.Add("john", new Play("King John", 2648, PlayType.History));
+        plays.Add("richard-iii", new Play("Richard III", 3718, PlayType.History));
+
+        Invoice invoice = new Invoice(
+            "BigCo",
+            new List<Performance>
+            {
+                new Performance(55, plays["hamlet"]),
+                new Performance(35, plays["as-like"]),
+                new Performance(40, plays["othello"]),
+                new Performance(20, plays["henry-v"]),
+                new Performance(39, plays["john"]),
+                new Performance(20, plays["henry-v"])
+            }
+        );
+
+        StatementPrinter statementPrinter = new StatementPrinter();
+        var result = statementPrinter.PrintXml(invoice);
 
         Approvals.Verify(result);
     }
