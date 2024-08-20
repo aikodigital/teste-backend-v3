3b1a9ec237022b14ae748efc4a6ae97eb3f5f299
Marlon Magalhaes <marlonmagalhaescar@hotmail.com>
2024-08-20 10:36:48 -0300

    Creation of the iplay interface

---
+++ TheatricalPlayersRefactoringKata/Calculators/IPlay.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheatricalPlayersRefactoringKata.Calculators
+{
+    public interface IPlay
+    {
+        double CalculateAmount(Play play, Performance performance);
+        int CalculateVolumeCredits(Performance performance);
+    }
+}
