817d3dc887e3483a339086ce8f72fa015e7673fd
Possari <pedropossari2@gmail.com>
2024-08-20 19:09:30 -0300

    TesteXML
    Foram adicionadas novas funcionalidades para a StatementePrinter.cs e StatementePrinterTests.cs
    Com o objetivo de realizar o teste do xml

--- TheatricalPlayersRefactoringKata.Tests/StatementPrinterTests.cs
+++ TheatricalPlayersRefactoringKata.Tests/StatementPrinterTests.cs
@@ -68,4 +68,37 @@ public class StatementPrinterTests
 
         Approvals.Verify(result);
     }
+
+    [Fact]
+    [UseReporter(typeof(DiffReporter))]
+    public void TestXmlStatementExample()
+    {
+        var plays = new Dictionary<string, Play>
+    {
+        { "hamlet", new Tragedy("Hamlet", 4024) },
+        { "as-like", new Comedy("As You Like It", 2670) },
+        { "othello", new Tragedy("Othello", 3560) },
+        { "henry-v", new Historic("Henry V", 3227) },
+        { "john", new Historic("King John", 2648) },
+        { "richard-iii", new Historic("Richard III", 3718) }
+    };
+
+        Invoice invoice = new Invoice(
+            "BigCo",
+            new List<Performance>
+            {
+            new Performance("hamlet", 55),
+            new Performance("as-like", 35),
+            new Performance("othello", 40),
+            new Performance("henry-v", 20),
+            new Performance("john", 39),
+            new Performance("henry-v", 20),
+            }
+        );
+
+        StatementPrinter statementPrinter = new StatementPrinter();
+        var result = statementPrinter.PrintXML(invoice, plays);
+
+        Approvals.Verify(result);
+    }
 }
--- TheatricalPlayersRefactoringKata/StatementPrinter.cs
+++ TheatricalPlayersRefactoringKata/StatementPrinter.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.IO;
+using System.Text;
+using System.Xml.Linq;
 
 namespace TheatricalPlayersRefactoringKata;
 
@@ -37,6 +40,68 @@ public class StatementPrinter
         result += String.Format(cultureInfo, "Amount owed is {0:C}\n", totalAmount);
         result += String.Format("You earned {0} credits\n", volumeCredits);
         return result;
+    }
+
+    public string PrintXML(Invoice invoice, Dictionary<string, Play> plays)
+    {
+        decimal totalAmount = 0;
+        int totalCredits = 0;
+
+        var items = new XElement("Items");
+
+        foreach (var perf in invoice.Performances)
+        {
+            var play = plays[perf.PlayId];
+
+            if (play is Historic historicPlay)
+            {
+                historicPlay.SetAudience(perf.Audience);
+            }
+            else
+            {
+                play.Audience = perf.Audience;
+            }
+
+            var thisAmount = play.CalculatorValor();
+            var playCredits = play.CalculatorCredits();
+
+            totalAmount += (decimal)thisAmount;
+            totalCredits += playCredits;
+
+            items.Add(new XElement("Item",
+            new XElement("AmountOwed", Convert.ToString(Convert.ToDecimal(thisAmount), CultureInfo.InvariantCulture)),
+            new XElement("EarnedCredits", playCredits),
+            new XElement("Seats", perf.Audience)
+        ));
+        }
+
+        var statement = new XElement("Statement",
+
+            new XAttribute(XNamespace.Xmlns + "xsi", "http://www.w3.org/2001/XMLSchema-instance"),
+            new XAttribute(XNamespace.Xmlns + "xsd", "http://www.w3.org/2001/XMLSchema"),
+            new XElement("Customer", invoice.Customer),
+            items,
+            new XElement("AmountOwed", totalAmount.ToString("0.0", CultureInfo.InvariantCulture)),
+            new XElement("EarnedCredits", totalCredits)
+        );
+
+        var xmlDocument = new XDocument(
+        new XDeclaration("1.0", "utf-8", null),
+        statement
+    );
+
+
+        using (var sw = new Utf8StringWriter())
+        {
+            xmlDocument.Save(sw);
+            return sw.ToString();
+        }
+    }
+
+
+    public class Utf8StringWriter : StringWriter
+    {
+        public override Encoding Encoding => Encoding.UTF8;
     }
 
 }
