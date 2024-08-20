a07e4d10cecf7cce17d90e28be996a794214da44
Marlon Magalhaes <marlonmagalhaescar@hotmail.com>
2024-08-20 10:39:02 -0300

    Creation of the Tragedy class

---
+++ TheatricalPlayersRefactoringKata/Calculators/Tragedy.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheatricalPlayersRefactoringKata.Calculators
+{
+    public class Tragedy : IPlay
+    {
+        public double CalculateAmount(Play play, Performance performance)
+        {
+            var lines = Math.Clamp(play.Lines, 1000, 4000);
+            var baseAmount = lines / 10.0;
+            if (performance.Audience > 30)
+            {
+                baseAmount += 10.0 * (performance.Audience - 30);
+            }
+            return baseAmount;
+        }
+
+        public int CalculateVolumeCredits(Performance performance)
+        {
+            return Math.Max(performance.Audience - 30, 0);
+        }
+    }
+}
