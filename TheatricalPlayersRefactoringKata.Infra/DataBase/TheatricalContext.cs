03d99306487594e732ed7281525e6c9a9cb2698c
Marcos <m.vinicius.sr11@gmail.com>
2024-08-20 16:53:45 -0300

    criação da camada de API

--- TheatricalPlayersRefactoringKata.Infra/DataBase/TheatricalContext.cs
+++ TheatricalPlayersRefactoringKata.Infra/DataBase/TheatricalContext.cs
@@ -8,9 +8,9 @@ namespace TheatricalPlayersRefactoringKata.Infra.DataBase
     {
         public TheatricalContext(DbContextOptions<TheatricalContext> options) : base(options) { }
 
-        public DbSet<Play> Plays { get; set; }
-        public DbSet<Performance> Performances { get; set; }
-        public DbSet<Invoice> Invoices { get; set; }
+        public DbSet<Play> Plays { get; set; } = null!;
+        public DbSet<Performance> Performances { get; set; } = null!;
+        public DbSet<Invoice> Invoices { get; set; } = null!;
 
         protected override void OnModelCreating(ModelBuilder mb)
         {
