f0d8f118ad224fdfe2e9dee7f2430c90161da545
Marcos <m.vinicius.sr11@gmail.com>
2024-08-20 22:56:40 -0300

    criação do TheatricalContextFactory.cs e adicionada connection string

---
+++ TheatricalPlayersRefactoringKata.Infra/DataBase/TheatricalContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace TheatricalPlayersRefactoringKata.Infra.DataBase
+{
+    public class TheatricalContextFactory : IDesignTimeDbContextFactory<TheatricalContext>
+    {
+        public TheatricalContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<TheatricalContext>();
+            optionsBuilder.UseMySQL("Server=localhost;Port=3307;User=root;Password=M@r1ll10n;Database=theatricalplayersdb");
+
+            return new TheatricalContext(optionsBuilder.Options);
+        }
+    }
+}
