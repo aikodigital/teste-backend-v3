92e0a5e77024d556ede4c98551c15ff90b29506b
pedroserretti <pedroserretti2@gmail.com>
2024-08-21 01:13:30 -0300

    feat: implementação da lógica do gênero histórico

--- TheatricalPlayersRefactoringKata.Domain/Core/Strategy/HistoricalGenreStrategy.cs
+++ TheatricalPlayersRefactoringKata.Domain/Core/Strategy/HistoricalGenreStrategy.cs
@@ -7,12 +7,25 @@ using System.Threading.Tasks;
 namespace TheatricalPlayersRefactoringKata.Domain.Core.Strategy {
     public class HistoricalGenreStrategy : IGenreStrategy {
 
+        private readonly TragedyGenreStrategy _tragedyGenreObj = new();
+        private readonly ComedyGenreStrategy _comedyGenreObj = new();
+
         public int CalculatePlayCredits(Performance perf) {
-            return 0;
+            int volumeCredits = Math.Max(perf.Audience - 30, 0);
+
+            return volumeCredits;
+
         }
 
         public double CalculatePlayAmount(Performance perf, double thisAmount) {
-            return 0;
+            double totalAmount = 0;
+
+            double tragedyAmount = _tragedyGenreObj.CalculatePlayAmount(perf, thisAmount);
+            double comedyAmount = _comedyGenreObj.CalculatePlayAmount(perf, thisAmount);
+
+            totalAmount = tragedyAmount + comedyAmount;
+
+            return totalAmount;
         }
     }
 }
