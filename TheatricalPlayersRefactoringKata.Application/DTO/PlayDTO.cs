7d97763730a156a69df27fe7f480b62bca737318
pedroserretti <pedroserretti2@gmail.com>
2024-08-23 01:07:48 -0300

    feat: criação de DTOs e classe de DbContext

---
+++ TheatricalPlayersRefactoringKata.Application/DTO/PlayDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheatricalPlayersRefactoringKata.Application.DTO
+{
+    public class PlayDTO
+    {
+        public string PlayName { get; set; }
+        public string AmountOwed { get; set; }
+        public string EarnedCredits { get; set; }
+        public string Seats { get; set; }
+
+        public PlayDTO(string playName, string amountOwed, string earnedCredits, string seats)
+        {
+            PlayName = playName;
+            AmountOwed = amountOwed;
+            EarnedCredits = earnedCredits;
+            Seats = seats;
+        }
+    }
+}
---
+++ TheatricalPlayersRefactoringKata.Infrastructure/Data/PrinterContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheatricalPlayersRefactoringKata.Application.DTO;
+
+namespace TheatricalPlayersRefactoringKata.Infrastructure.Data
+{
+    public class PrinterContext : DbContext {
+
+        public PrinterContext(DbContextOptions<PrinterContext> options) : base(options) { }
+
+        public DbSet<StatementDTO> Statement { get; set; }
+
+    }
+}
