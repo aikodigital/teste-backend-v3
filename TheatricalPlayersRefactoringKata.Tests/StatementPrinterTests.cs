a2003c0bc3612d0b2b9bd3091b1b5961ef5e5b06
rafarafael <rl.oliveira321@gmail.com>
2024-08-26 01:58:35 -0300

    xml e teste xml

--- TheatricalPlayersRefactoringKata.Tests/StatementPrinterTests.cs
+++ TheatricalPlayersRefactoringKata.Tests/StatementPrinterTests.cs
@@ -205,4 +205,29 @@ public class StatementPrinterTests
 
         Approvals.Verify(result);
     }
+    
+    [Fact]
+    [UseReporter(typeof(DiffReporter))]
+    public void TestPrintXml()
+    {
+        var plays = new Dictionary<string, Play>
+    {
+        { "hamlet", new Play("Hamlet", 4024, "tragedy") },
+        { "as-like", new Play("As You Like It", 2670, "comedy") }
+    };
+
+        Invoice invoice = new Invoice(
+            "BigCo",
+            new List<Performance>
+            {
+            new Performance("hamlet", 55),
+            new Performance("as-like", 35)
+            }
+        );
+
+        StatementPrinter statementPrinter = new StatementPrinter();
+        var result = statementPrinter.PrintXml(invoice, plays);
+
+        Approvals.Verify(result);
+    }
 }
--- TheatricalPlayersRefactoringKata/StatementPrinter.cs
+++ TheatricalPlayersRefactoringKata/StatementPrinter.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Text;
+using System.Xml.Linq;
 
 namespace TheatricalPlayersRefactoringKata
 {
@@ -10,8 +12,10 @@ namespace TheatricalPlayersRefactoringKata
         {
             var totalAmount = 0M;
             var volumeCredits = 0;
-            var result = string.Format("Statement for {0}\n", invoice.Customer);
-            CultureInfo cultureInfo = new CultureInfo("en-US");
+            var result = new StringBuilder();
+            var cultureInfo = new CultureInfo("en-US");
+
+            result.AppendFormat("Statement for {0}\n", invoice.Customer);
 
             foreach (var perf in invoice.Performances)
             {
@@ -40,26 +44,86 @@ namespace TheatricalPlayersRefactoringKata
                 if ("comedy" == play.Type) volumeCredits += (int)Math.Floor((decimal)perf.Audience / 5);
 
                 // print line for this order
-                result += String.Format(cultureInfo, "  {0}: {1:C} ({2} seats)\n", play.Name, thisAmount, perf.Audience);
+                result.AppendFormat(cultureInfo, "  {0}: {1:C} ({2} seats)\n", play.Name, thisAmount, perf.Audience);
                 totalAmount += thisAmount;
             }
-            result += String.Format(cultureInfo, "Amount owed is {0:C}\n", totalAmount);
-            result += String.Format("You earned {0} credits\n", volumeCredits);
-            return result;
+            result.AppendFormat(cultureInfo, "Amount owed is {0:C}\n", totalAmount);
+            result.AppendFormat("You earned {0} credits\n", volumeCredits);
+            return result.ToString();
+        }
+
+        public string PrintXml(Invoice invoice, Dictionary<string, Play> plays)
+        {
+            var doc = new XDocument(new XElement("Statement",
+                new XElement("Customer", invoice.Customer),
+                new XElement("TotalAmount", "0"),
+                new XElement("VolumeCredits", "0"),
+                new XElement("Performances")
+            ));
+
+            var totalAmount = 0M;
+            var volumeCredits = 0;
+
+            foreach (var perf in invoice.Performances)
+            {
+                var play = plays[perf.PlayId];
+                var lines = play.Lines;
+                if (lines < 1000) lines = 1000;
+                if (lines > 4000) lines = 4000;
+                var thisAmount = lines * 10M;
+                switch (play.Type)
+                {
+                    case "tragedy":
+                        thisAmount = CalculateTragedyAmount(lines, perf.Audience);
+                        break;
+                    case "comedy":
+                        thisAmount = CalculateComedyAmount(lines, perf.Audience);
+                        break;
+                    case "history":
+                        thisAmount = CalculateHistoryAmount(lines, perf.Audience);
+                        break;
+                    default:
+                        throw new Exception("unknown type: " + play.Type);
+                }
+                // add volume credits
+                volumeCredits += Math.Max(perf.Audience - 30, 0);
+                // add extra credit for every ten comedy attendees
+                if ("comedy" == play.Type) volumeCredits += (int)Math.Floor((decimal)perf.Audience / 5);
+
+                totalAmount += thisAmount;
+
+                doc.Root.Element("Performances").Add(new XElement("Performance",
+                    new XElement("Name", play.Name),
+                    new XElement("Amount", FormatCurrency(thisAmount)),
+                    new XElement("Seats", perf.Audience)
+                ));
+            }
+
+            doc.Root.Element("TotalAmount").Value = FormatCurrency(totalAmount);
+            doc.Root.Element("VolumeCredits").Value = volumeCredits.ToString();
+
+            return doc.ToString();
+        }
+
+        private string FormatCurrency(decimal amount)
+        {
+            var cultureInfo = new CultureInfo("en-US");
+            return amount.ToString("C", cultureInfo);
         }
         private decimal CalculateTragedyAmount(int lines, int audience)
         {
             var baseAmount = lines * 10M;
             var extraAmount = audience > 30 ? 1000M * (audience - 30) : 0M;
-            return (baseAmount + extraAmount)/100;
+            return (baseAmount + extraAmount) / 100;
         }
 
         private decimal CalculateComedyAmount(int lines, int audience)
         {
             var baseAmount = lines * 10M;
             var extraAmount = audience > 20 ? 10000M + 500M * (audience - 20) : 0M;
-            return (baseAmount + extraAmount + 300M * audience)/100;
+            return (baseAmount + extraAmount + 300M * audience) / 100;
         }
+
         private decimal CalculateHistoryAmount(int lines, int audience)
         {
             return CalculateTragedyAmount(lines, audience) + CalculateComedyAmount(lines, audience);
