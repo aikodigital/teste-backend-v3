5ff82d753c18ae80ba43d7c3a888ae2101a537b1
luzalex23 <alexluzendreyravi@gmail.com>
2024-08-27 22:02:44 -0300

    fix: udpade dotnet version 6 to 8

--- TheatricalPlayersRefactoringKata.Tests/StatementPrinterTests.cs
+++ TheatricalPlayersRefactoringKata.Tests/StatementPrinterTests.cs
@@ -1,7 +1,6 @@
-using System;
-using System.Collections.Generic;
 using ApprovalTests;
 using ApprovalTests.Reporters;
+using System.Collections.Generic;
 using Xunit;
 
 namespace TheatricalPlayersRefactoringKata.Tests;
