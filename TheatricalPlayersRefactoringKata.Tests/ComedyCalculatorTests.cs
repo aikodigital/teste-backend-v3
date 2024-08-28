c70e035eee10f3a7a1217039d7b7779ed756f104
Eduardo <eduardoj.schroeder@gmail.com>
2024-08-28 15:00:32 -0300

    Commit com a adição de testes unitários para cada gênero.

---
+++ TheatricalPlayersRefactoringKata.Tests/ComedyCalculatorTests.cs
@@ -0,0 +1,53 @@
+﻿using TheatricalPlayersRefactoringKata.Models;
+using TheatricalPlayersRefactoringKata.Services;
+using Xunit;
+
+namespace TheatricalPlayersRefactoringKata.Tests;
+
+public class ComedyCalculatorTests
+{
+    [Fact]
+    public void CalculateAmount_ShouldReturnCorrectAmount_WhenAudienceIsLessThanOrEqualTo20()
+    {
+        // Arrange
+        var calculator = new ComedyCalculator();
+        var play = new Play("test", 2000, "comedy");
+        var performance = new Performance("test", 20);
+
+        // Act
+        var amount = calculator.CalculateAmount(performance, play);
+
+        // Assert
+        Assert.Equal(26000, amount);
+    }
+
+    [Fact]
+    public void CalculateAmount_ShouldReturnCorrectAmount_WhenAudienceIsGreaterThan20()
+    {
+        // Arrange
+        var calculator = new ComedyCalculator();
+        var play = new Play("test", 2000, "comedy");
+        var performance = new Performance("test", 30);
+
+        // Act
+        var amount = calculator.CalculateAmount(performance, play);
+
+        // Assert
+        Assert.Equal(44000, amount);
+    }
+
+    [Fact]
+    public void CalculateVolumeCredits_ShouldReturnCorrectCredits()
+    {
+        // Arrange
+        var calculator = new ComedyCalculator();
+        var performance = new Performance("test", 40);
+
+        // Act
+        var credits = calculator.CalculateVolumeCredits(performance, null);
+
+        // Assert
+        Assert.Equal(18, credits);
+    }
+}
+
---
+++ TheatricalPlayersRefactoringKata.Tests/HistoryCalculatorTests.cs
@@ -0,0 +1,67 @@
+﻿using TheatricalPlayersRefactoringKata.Models;
+using TheatricalPlayersRefactoringKata.Services;
+using Xunit;
+
+namespace TheatricalPlayersRefactoringKata.Tests;
+
+public class HistoryCalculatorTests
+{
+    [Fact]
+    public void CalculateAmount_ShouldReturnCorrectAmount_ForMixedAudience()
+    {
+        // Arrange
+        var calculator = new HistoryCalculator();
+        var play = new Play("test", 3000, "history");
+        var performance = new Performance("test", 39);
+
+        // Act
+        var amount = calculator.CalculateAmount(performance, play);
+
+        // Assert
+        Assert.Equal(100200, amount);
+    }
+
+    [Fact]
+    public void CalculateVolumeCredits_ShouldReturnCorrectCredits()
+    {
+        // Arrange
+        var calculator = new HistoryCalculator();
+        var performance = new Performance ("test", 39);
+
+        // Act
+        var credits = calculator.CalculateVolumeCredits(performance, null);
+
+        // Assert
+        Assert.Equal(9, credits);
+    }
+
+    [Fact]
+    public void CalculateAmount_ShouldReturnCorrectAmount_ForLowerAudience()
+    {
+        // Arrange
+        var calculator = new HistoryCalculator();
+        var play = new Play("test", 2000, "history");
+        var performance = new Performance("test", 25);
+
+        // Act
+        var amount = calculator.CalculateAmount(performance, play);
+
+        // Assert
+        Assert.Equal(60000, amount);
+    }
+
+    [Fact]
+    public void CalculateVolumeCredits_ShouldReturnCorrectCredits_ForLowerAudience()
+    {
+        // Arrange
+        var calculator = new HistoryCalculator();
+        var performance = new Performance("test", 25);
+
+        // Act
+        var credits = calculator.CalculateVolumeCredits(performance, null);
+
+        // Assert
+        Assert.Equal(0, credits);
+    }
+}
+
