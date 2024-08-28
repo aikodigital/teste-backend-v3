585ba507d261040ae7bb9ed87e424f02b593afa4
luzalex23 <alexluzendreyravi@gmail.com>
2024-08-28 14:12:55 -0300

    feat: Adding Custom Exceptions

---
+++ src/Shared/TheatricalPlayersRefactoringKata.Exception/BaseException/CustomizedException.cs
@@ -0,0 +1,7 @@
+﻿namespace TheatricalPlayersRefactoringKata.Exception.BaseException;
+
+public class CustomizedException : System.Exception
+{
+    public CustomizedException(string message) : base(message) { }
+
+}
---
+++ src/Shared/TheatricalPlayersRefactoringKata.Exception/BaseException/ErrorOnValidationException.cs
@@ -0,0 +1,12 @@
+﻿namespace TheatricalPlayersRefactoringKata.Exception.BaseException;
+
+public class ErrorOnValidationException : CustomizedException
+{
+    public List<string> ErrorMessage { get; set; }
+
+    public ErrorOnValidationException(List<string> errorMessages) : base(string.Empty)
+    {
+        ErrorMessage = errorMessages;
+
+    }
+}
