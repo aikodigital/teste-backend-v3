1cf6ee00b6bdca975173c9bfe80ddcb8f3c3de5a
TakeshiYoshikawa <takeshiy096@gmail.com>
2024-08-28 17:38:10 -0300

    Refactor project structure to align with Domain-Driven Design (DDD) principles.

---
+++ TheatricalPlayersRefactoringKata/Interfaces/IPlayTypeCalculatorStrategy.cs
@@ -0,0 +1,10 @@
+﻿using TheatricalPlayersRefactoringKata.Entities;
+
+namespace TheatricalPlayersRefactoringKata.Interfaces
+{
+    public interface IPlayTypeCalculatorStrategy
+    {
+        double CalculateAmount(Play play, Performance performance);
+        int CalculateCredit(Performance performance);
+    }
+}
--- TheatricalPlayersRefactoringKata/StatementPrinter.cs
+++ TheatricalPlayersRefactoringKata/StatementPrinter.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
+using TheatricalPlayersRefactoringKata.Entities;
 
 namespace TheatricalPlayersRefactoringKata;
 
