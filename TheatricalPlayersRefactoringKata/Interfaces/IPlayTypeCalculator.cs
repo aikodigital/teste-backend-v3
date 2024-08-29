4cfb4c7c3b17b176c4803c1195a3e4cff7941eb6
TakeshiYoshikawa <takeshiy096@gmail.com>
2024-08-28 23:01:49 -0300

    Renamed IPlayTypeCalculatorStrategy to IPlayTypeCalculator.

---
+++ TheatricalPlayersRefactoringKata/Interfaces/IPlayTypeCalculator.cs
@@ -0,0 +1,10 @@
+﻿using TheatricalPlayersRefactoringKata.Entities;
+
+namespace TheatricalPlayersRefactoringKata.Interfaces
+{
+    public interface IPlayTypeCalculator
+    {
+        decimal CalculateAmount(Play play, Performance performance);
+        int CalculateCredits(Performance performance);
+    }
+}
