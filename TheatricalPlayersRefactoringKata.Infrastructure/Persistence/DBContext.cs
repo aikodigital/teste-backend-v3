823caf2f74e982a9ba69bf9ed90b8ea34c9b61c3
TakeshiYoshikawa <takeshiy096@gmail.com>
2024-08-30 17:49:19 -0300

    Setup StringConnection between API and PostgreSQL

---
+++ TheatricalPlayersRefactoringKata.Infrastructure/Persistence/DBContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheatricalPlayersRefactoringKata.Infrastructure.Persistence
+{
+    public class DBContext : DbContext
+    {
+        private IConfiguration _config;
+
+        public DBContext(IConfiguration config, DbContextOptions options) : base(options)
+        {
+            _config = config;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = _config.GetConnectionString("npgServer"); 
+            optionsBuilder.UseNpgsql(connectionString);
+        }
+
+    }
+}
