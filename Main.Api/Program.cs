8c5901d966fffd5865f0494593a6d9365e91cc30
Gabriel Ribeiro <gabrielrm2000@hotmail.com>
2024-09-01 19:27:12 -0300

    Adicionado Swagger

--- Main.Api/Program.cs
+++ Main.Api/Program.cs
@@ -1,6 +1,8 @@
 using Main.Application;
 using Main.Infrastructure;
 using Main.Domain;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 {
     builder.Services
@@ -8,10 +10,21 @@ var builder = WebApplication.CreateBuilder(args);
         .AddInfrastructure()
         .AddDomain();
     builder.Services.AddControllers();
+
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen(c =>
+    {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "TheatricalPlayersRefactoringKata", Version = "v1" });
+    });
 }
 
 var app = builder.Build();
 {
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "TheatricalPlayersRefactoringKata v1");
+    });
     app.UseHttpsRedirection();
     app.MapControllers();
     app.Run();
