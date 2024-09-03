3f412ec3ba0f06897ee321633a158dd1c340ff71
Guilherme Almeida <guimathiasgui@gmail.com>
2024-09-02 22:24:39 -0300

    Refatorando classe StatementPrinter

--- TheatricalPlayersRefactoringKata.Tests/StatementPrinterTests.cs
+++ TheatricalPlayersRefactoringKata.Tests/StatementPrinterTests.cs
@@ -12,10 +12,12 @@ public class StatementPrinterTests
     [UseReporter(typeof(DiffReporter))]
     public void TestStatementExampleLegacy()
     {
-        var plays = new Dictionary<string, Play>();
-        plays.Add("hamlet", new Play("Hamlet", 4024, "tragedy"));
-        plays.Add("as-like", new Play("As You Like It", 2670, "comedy"));
-        plays.Add("othello", new Play("Othello", 3560, "tragedy"));
+        var plays = new Dictionary<string, Play>
+        {
+            { "hamlet", new Play("Hamlet", 4024, "tragedy") },
+            { "as-like", new Play("As You Like It", 2670, "comedy") },
+            { "othello", new Play("Othello", 3560, "tragedy") }
+        };
 
         Invoice invoice = new Invoice(
             "BigCo",
@@ -37,13 +39,15 @@ public class StatementPrinterTests
     [UseReporter(typeof(DiffReporter))]
     public void TestTextStatementExample()
     {
-        var plays = new Dictionary<string, Play>();
-        plays.Add("hamlet", new Play("Hamlet", 4024, "tragedy"));
-        plays.Add("as-like", new Play("As You Like It", 2670, "comedy"));
-        plays.Add("othello", new Play("Othello", 3560, "tragedy"));
-        plays.Add("henry-v", new Play("Henry V", 3227, "history"));
-        plays.Add("john", new Play("King John", 2648, "history"));
-        plays.Add("richard-iii", new Play("Richard III", 3718, "history"));
+        var plays = new Dictionary<string, Play>
+        {
+            { "hamlet", new Play("Hamlet", 4024, "tragedy") },
+            { "as-like", new Play("As You Like It", 2670, "comedy") },
+            { "othello", new Play("Othello", 3560, "tragedy") },
+            { "henry-v", new Play("Henry V", 3227, "history") },
+            { "john", new Play("King John", 2648, "history") },
+            { "richard-iii", new Play("Richard III", 3718, "history") }
+        };
 
         Invoice invoice = new Invoice(
             "BigCo",
