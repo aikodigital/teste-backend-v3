479be36a9e95b635b521698d20236af47c5a1375
leticiamattoso <56324707+leticiamattoso@users.noreply.github.com>
2024-09-14 14:10:01 -0300

    FEAT: Criar arquitetura clean.
    Realizado a criação de uma arquitetura clean em camadas net 8.0.

---
+++ API/Domain/TS.Domain/EntityFramework/AppDbContext.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheatricalPlayersRefactoringKata.Domain.TS.Domain.EntityFramework
+{
+    public class AppDbContext
+    {
+        
+    }
+}
