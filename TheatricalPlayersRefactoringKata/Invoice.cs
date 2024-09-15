6c2b1db262282a08ea201b689583b7e7530adc6d
Gabriel Souza <57289760+gabrielGaates@users.noreply.github.com>
2024-09-15 00:14:57 -0300

    Adicionado chamada de cálculo consolidado na classe Invoice.

--- TheatricalPlayersRefactoringKata/Invoice.cs
+++ TheatricalPlayersRefactoringKata/Invoice.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 
 namespace TheatricalPlayersRefactoringKata;
 
@@ -8,7 +9,25 @@ public class Invoice
     private List<Performance> _performances;
 
     public string Customer { get => _customer; set => _customer = value; }
-    public List<Performance> Performances { get => _performances; set => _performances = value; }
+    public List<Performance> Performances
+    {
+        get => _performances;
+        set
+        {
+            _performances = value;
+         
+        }
+    }
+
+
+    public decimal CalculateTotals()
+    {
+        return _performances  != null ? _performances.Sum(perf => perf.CalculateValue()) : 0;
+    }
+    public int CalculateCredits()
+    {
+        return _performances != null ? _performances.Sum(perf => perf.CalculateCredits()) : 0;
+    }
 
     public Invoice(string customer, List<Performance> performance)
     {
--- TheatricalPlayersRefactoringKata/StatementPrinter.cs
+++ TheatricalPlayersRefactoringKata/StatementPrinter.cs
@@ -8,22 +8,19 @@ public class StatementPrinter
 {
     public string Print(Invoice invoice)
     {
-        decimal totalAmount = 0;
-        var volumeCredits = 0;
         var result = string.Format("Statement for {0}\n", invoice.Customer);
         CultureInfo cultureInfo = new CultureInfo("en-US");
 
-        foreach(var perf in invoice.Performances) 
+        decimal totalAmount = invoice.CalculateTotals();
+        var volumeCredits = invoice.CalculateCredits();
+
+        foreach (var perf in invoice.Performances) 
         {
 
             var thisAmount = perf.CalculateValue();
 
-            // add volume credits
-            volumeCredits += perf.CalculateCredits();
-
             // print line for this order
             result += String.Format(cultureInfo, "  {0}: {1:C} ({2} seats)\n", perf.play.Name, Convert.ToDecimal(thisAmount / 100), perf.Audience);
-            totalAmount += thisAmount;
         }
         result += String.Format(cultureInfo, "Amount owed is {0:C}\n", Convert.ToDecimal(totalAmount / 100));
         result += String.Format("You earned {0} credits\n", volumeCredits);
