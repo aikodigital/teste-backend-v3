cad287d85c75edb2f848897a4d8e43da08f0e908
Luiz Henrique da Costa <henrique.verde@hotmail.com>
2024-09-15 00:05:10 -0300

    Criação de constantes para melhor organização e entendimento do Código, assim retirando número fixos da regra de negócio e centralizando em um única classe.

---
+++ TheatricalPlayersRefactoringKata.Application/Constants/StatementPrinterConstants.cs
@@ -0,0 +1,31 @@
+﻿namespace TheatricalPlayersRefactoringKata.Application.Constants
+{
+    public static class StatementPrinterConstants
+    {
+        #region General
+        public const int MINIMUM_LINES = 1000;
+        public const int MAXIMUM_LINES = 4000;
+        public const decimal DIVIDER_PER_LINE = 10;
+        public const int CREDIT_MINIMUM_AUDIENCE = 30;
+        #endregion
+        #region Tragedy
+        public const int TRAGEDY_MINIMUM_AUDIENCE = 30;
+        public const decimal TRAGEDY_PER_AUDIENCE_ADDITIONAL = 10.00m;
+        #endregion
+        #region Comedy
+        public const int COMEDY_MINIMUM_AUDIENCE = 20;
+        public const decimal COMEDY_PER_AUDIENCE = 3.00m;
+        public const decimal COMEDY_BONUS = 100.00m;
+        public const decimal COMEDY_PER_AUDIENCE_ADDITIONAL = 5.00m;
+        public const decimal COMEDY_BONUS_CREDIT_PER_ATTENDEES = 5m;
+        #endregion
+
+
+
+
+        //public const decimal BASE_AMOUNT_FOR_PLAY = 100.00m;
+        //public const decimal BONUS_CREDITS_THRESHOLD = 30;
+        
+        
+    }
+}
--- TheatricalPlayersRefactoringKata.Application/Services/StatementPrinter.cs
+++ TheatricalPlayersRefactoringKata.Application/Services/StatementPrinter.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Globalization;
 using TheatricalPlayersRefactoringKata.Domain;
+using TheatricalPlayersRefactoringKata.Application.Constants;
 
 namespace TheatricalPlayersRefactoringKata.Application;
 
@@ -14,40 +15,63 @@ public class StatementPrinter
         var result = string.Format("Statement for {0}\n", invoice.Customer);
         CultureInfo cultureInfo = new CultureInfo("en-US");
 
-        foreach(var perf in invoice.Performances) 
+        foreach (var performance in invoice.Performances)
         {
-            var play = plays[perf.PlayId];
+            var play = plays[performance.PlayId];
             var lines = play.Lines;
-            if (lines < 1000) lines = 1000;
-            if (lines > 4000) lines = 4000;
-            var thisAmount = lines / 10m;
+            if (lines < StatementPrinterConstants.MINIMUM_LINES) lines = StatementPrinterConstants.MINIMUM_LINES;
+            if (lines > StatementPrinterConstants.MAXIMUM_LINES) lines = StatementPrinterConstants.MAXIMUM_LINES;
+            var thisAmount = lines / StatementPrinterConstants.DIVIDER_PER_LINE;
 
-            switch (play.Type) 
+            switch (play.Type)
             {
                 case "tragedy":
-                    if (perf.Audience > 30) {
-                        thisAmount += 10.00m * (perf.Audience - 30);
+                    if (performance.Audience > StatementPrinterConstants.TRAGEDY_MINIMUM_AUDIENCE)
+                    {
+                        thisAmount += StatementPrinterConstants.TRAGEDY_PER_AUDIENCE_ADDITIONAL 
+                                      * (performance.Audience - StatementPrinterConstants.TRAGEDY_MINIMUM_AUDIENCE);
                     }
                     break;
                 case "comedy":
-                    if (perf.Audience > 20) {
-                        thisAmount += 100.00m + 5.00m * (perf.Audience - 20);
+                    if (performance.Audience > StatementPrinterConstants.COMEDY_MINIMUM_AUDIENCE)
+                    {
+                        thisAmount += StatementPrinterConstants.COMEDY_BONUS 
+                                      + StatementPrinterConstants.COMEDY_PER_AUDIENCE_ADDITIONAL
+                                      * (performance.Audience - StatementPrinterConstants.COMEDY_MINIMUM_AUDIENCE);
                     }
-                    thisAmount += 3.00m * perf.Audience;
+                    thisAmount += StatementPrinterConstants.COMEDY_PER_AUDIENCE * performance.Audience;
+                    break;
+                case "history":
+                    var tragedyAmount = (lines / StatementPrinterConstants.DIVIDER_PER_LINE);
+                    if (performance.Audience > StatementPrinterConstants.TRAGEDY_MINIMUM_AUDIENCE)
+                    {
+                        tragedyAmount += StatementPrinterConstants.TRAGEDY_PER_AUDIENCE_ADDITIONAL
+                                      * (performance.Audience - StatementPrinterConstants.TRAGEDY_MINIMUM_AUDIENCE);
+                    }
+
+                    var comedyAmount = (lines / StatementPrinterConstants.DIVIDER_PER_LINE);
+                    if (performance.Audience > StatementPrinterConstants.COMEDY_MINIMUM_AUDIENCE)
+                    {
+                        comedyAmount += StatementPrinterConstants.COMEDY_BONUS
+                                      + StatementPrinterConstants.COMEDY_PER_AUDIENCE_ADDITIONAL
+                                      * (performance.Audience - StatementPrinterConstants.COMEDY_MINIMUM_AUDIENCE);
+                    }
+                    comedyAmount += StatementPrinterConstants.COMEDY_PER_AUDIENCE * performance.Audience;
+                    thisAmount = tragedyAmount + comedyAmount;
                     break;
                 default:
                     throw new Exception("unknown type: " + play.Type);
             }
             // add volume credits
-            volumeCredits += Math.Max(perf.Audience - 30, 0);
+            volumeCredits += Math.Max(performance.Audience - StatementPrinterConstants.CREDIT_MINIMUM_AUDIENCE, 0);
             // add extra credit for every ten comedy attendees
-            if ("comedy" == play.Type) volumeCredits += (int)Math.Floor(perf.Audience / 5m);
+            if ("comedy" == play.Type) volumeCredits += (int)Math.Floor(performance.Audience / StatementPrinterConstants.COMEDY_BONUS_CREDIT_PER_ATTENDEES);
 
             // print line for this order
-            result += String.Format(cultureInfo, "  {0}: {1:C} ({2} seats)\n", play.Name, Convert.ToDecimal(thisAmount / 100), perf.Audience);
+            result += String.Format(cultureInfo, "  {0}: {1:C} ({2} seats)\n", play.Name, thisAmount, performance.Audience);
             totalAmount += thisAmount;
         }
-        result += String.Format(cultureInfo, "Amount owed is {0:C}\n", Convert.ToDecimal(totalAmount / 100));
+        result += String.Format(cultureInfo, "Amount owed is {0:C}\n", totalAmount);
         result += String.Format("You earned {0} credits\n", volumeCredits);
         return result;
     }
