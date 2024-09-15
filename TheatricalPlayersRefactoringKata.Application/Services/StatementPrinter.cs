5d41c78d5cbabeaa8fb75a3ed84c31a9d4f7d3a5
Luiz Henrique da Costa <henrique.verde@hotmail.com>
2024-09-14 23:04:10 -0300

    Correções na regra de negócio.

--- TheatricalPlayersRefactoringKata.Application/Services/StatementPrinter.cs
+++ TheatricalPlayersRefactoringKata.Application/Services/StatementPrinter.cs
@@ -9,8 +9,8 @@ public class StatementPrinter
 {
     public string Print(Invoice invoice, Dictionary<string, Play> plays)
     {
-        var totalAmount = 0;
-        var volumeCredits = 0;
+        var totalAmount = 0m;
+        var volumeCredits = 0m;
         var result = string.Format("Statement for {0}\n", invoice.Customer);
         CultureInfo cultureInfo = new CultureInfo("en-US");
 
@@ -20,19 +20,20 @@ public class StatementPrinter
             var lines = play.Lines;
             if (lines < 1000) lines = 1000;
             if (lines > 4000) lines = 4000;
-            var thisAmount = lines * 10;
+            var thisAmount = lines / 10m;
+
             switch (play.Type) 
             {
                 case "tragedy":
                     if (perf.Audience > 30) {
-                        thisAmount += 1000 * (perf.Audience - 30);
+                        thisAmount += 10.00m * (perf.Audience - 30);
                     }
                     break;
                 case "comedy":
                     if (perf.Audience > 20) {
-                        thisAmount += 10000 + 500 * (perf.Audience - 20);
+                        thisAmount += 100.00m + 5.00m * (perf.Audience - 20);
                     }
-                    thisAmount += 300 * perf.Audience;
+                    thisAmount += 3.00m * perf.Audience;
                     break;
                 default:
                     throw new Exception("unknown type: " + play.Type);
@@ -40,7 +41,7 @@ public class StatementPrinter
             // add volume credits
             volumeCredits += Math.Max(perf.Audience - 30, 0);
             // add extra credit for every ten comedy attendees
-            if ("comedy" == play.Type) volumeCredits += (int)Math.Floor((decimal)perf.Audience / 5);
+            if ("comedy" == play.Type) volumeCredits += (int)Math.Floor(perf.Audience / 5m);
 
             // print line for this order
             result += String.Format(cultureInfo, "  {0}: {1:C} ({2} seats)\n", play.Name, Convert.ToDecimal(thisAmount / 100), perf.Audience);
