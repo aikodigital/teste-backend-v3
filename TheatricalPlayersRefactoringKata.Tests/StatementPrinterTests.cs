6df723ec006c1bfa52e3e029764acd5f23c21618
lucasvil4r <lucasvilar-celestino@hotmail.com>
2024-09-16 22:00:03 -0300

    test: reecriando os testes com a mesma lógica inicial, porém com os novos métodos adicionados

--- TheatricalPlayersRefactoringKata.Tests/StatementPrinterTests.cs
+++ TheatricalPlayersRefactoringKata.Tests/StatementPrinterTests.cs
@@ -9,7 +9,6 @@ namespace TheatricalPlayersRefactoringKata.Tests;
 
 public class StatementPrinterTests
 {
-    /* #deprecated
     [Fact]
     [UseReporter(typeof(DiffReporter))]
     public void TestStatementExampleLegacy()
@@ -29,71 +28,7 @@ public class StatementPrinterTests
             }
         );
 
-        //StatementPrinter statementPrinter = new StatementPrinter();
-        //var result = statementPrinter.Print(invoice, plays);
-
-        //Approvals.Verify(result);
-    }
-    */
-
-    /* #deprecated
-    [Fact]
-    [UseReporter(typeof(DiffReporter))]
-    public void TestTextStatementExample()
-    {
-        var plays = new Dictionary<string, Play>();
-        plays.Add("hamlet", new Play("Hamlet", 4024, "tragedy"));
-        plays.Add("as-like", new Play("As You Like It", 2670, "comedy"));
-        plays.Add("othello", new Play("Othello", 3560, "tragedy"));
-        plays.Add("henry-v", new Play("Henry V", 3227, "history"));
-        plays.Add("john", new Play("King John", 2648, "history"));
-        plays.Add("richard-iii", new Play("Richard III", 3718, "history"));
-
-        Invoice invoice = new Invoice(
-            "BigCo",
-            new List<Performance>
-            {
-                new Performance("hamlet", 55),
-                new Performance("as-like", 35),
-                new Performance("othello", 40),
-                new Performance("henry-v", 20),
-                new Performance("john", 39),
-                new Performance("henry-v", 20)
-            }
-        );
-
-        StatementPrinter statementPrinter = new StatementPrinter(); 
-        var result = statementPrinter.Print(invoice, plays);
-
-        Approvals.Verify(result);
-    }
-    */
-
-    [Fact]
-    [UseReporter(typeof(DiffReporter))]
-    public void TestTextStatementExample()
-    {
-        var plays = new Dictionary<string, Play>();
-        plays.Add("hamlet", new Play("Hamlet", 4024, "tragedy"));
-        plays.Add("as-like", new Play("As You Like It", 2670, "comedy"));
-        plays.Add("othello", new Play("Othello", 3560, "tragedy"));
-        plays.Add("henry-v", new Play("Henry V", 3227, "history"));
-        plays.Add("john", new Play("King John", 2648, "history"));
-        plays.Add("richard-iii", new Play("Richard III", 3718, "history"));
-
-        Invoice invoice = new Invoice(
-            "BigCo",
-            new List<Performance>
-            {
-                new Performance("hamlet", 55),
-                new Performance("as-like", 35),
-                new Performance("othello", 40),
-                new Performance("henry-v", 20),
-                new Performance("john", 39),
-                new Performance("henry-v", 20)
-            }
-        );
-
+        //StatementPrinter statementPrinter = new StatementPrinter(); deprecated
         TextStatementPrinter statementPrinter = new TextStatementPrinter();
         var result = statementPrinter.Print(invoice, plays);
 
@@ -101,7 +36,39 @@ public class StatementPrinterTests
     }
 
     [Fact]
-    [UseReporter(typeof(DiffReporter))]  // Define qual reporter usar para mostrar as diferenças
+    [UseReporter(typeof(DiffReporter))]
+    public void TestTextStatementExample()
+    {
+        var plays = new Dictionary<string, Play>();
+        plays.Add("hamlet", new Play("Hamlet", 4024, "tragedy"));
+        plays.Add("as-like", new Play("As You Like It", 2670, "comedy"));
+        plays.Add("othello", new Play("Othello", 3560, "tragedy"));
+        plays.Add("henry-v", new Play("Henry V", 3227, "history"));
+        plays.Add("john", new Play("King John", 2648, "history"));
+        plays.Add("richard-iii", new Play("Richard III", 3718, "history"));
+
+        Invoice invoice = new Invoice(
+            "BigCo",
+            new List<Performance>
+            {
+                new Performance("hamlet", 55),
+                new Performance("as-like", 35),
+                new Performance("othello", 40),
+                new Performance("henry-v", 20),
+                new Performance("john", 39),
+                new Performance("henry-v", 20)
+            }
+        );
+
+        //StatementPrinter statementPrinter = new StatementPrinter(); deprecated
+        TextStatementPrinter statementPrinter = new TextStatementPrinter();
+        var result = statementPrinter.Print(invoice, plays);
+
+        Approvals.Verify(result);
+    }
+
+    [Fact]
+    [UseReporter(typeof(DiffReporter))]
     public void TestXmlStatementExample()
     {
         var plays = new Dictionary<string, Play>();
@@ -125,11 +92,9 @@ public class StatementPrinterTests
             }
         );
 
-        // Gerando o resultado da impressão XML
         XmlStatementPrinter statementPrinter = new XmlStatementPrinter();
         var result = statementPrinter.Print(invoice, plays);
 
-        // Verificando o resultado gerado usando Approvals
         Approvals.Verify(result);
     }
 }
