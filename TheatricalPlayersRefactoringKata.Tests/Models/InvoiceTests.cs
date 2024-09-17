2f4f40caa1ed3f5a50dbbcd1d55f2524d4b5774a
Alisson Kauan <alisson.kauan77@gmail.com>
2024-09-17 13:50:32 -0300

    Unit tests created for Models and Factories and reorganization of the testing project.

---
+++ TheatricalPlayersRefactoringKata.Tests/Models/InvoiceTests.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using Xunit;
+using TheatricalPlayersRefactoringKata.Models;
+
+namespace TheatricalPlayersRefactoringKata.Tests
+{
+    public class InvoiceTests
+    {
+        [Fact]
+        public void Constructor_ShouldInitializePropertiesCorrectly()
+        {
+            var customer = "BigCo";
+            var performances = new List<Performance>
+            {
+                new Performance("hamlet", 55),
+                new Performance("as-like", 35)
+            };
+
+            var invoice = new Invoice(customer, performances);
+
+            Assert.Equal(customer, invoice.Customer);
+            Assert.Equal(performances, invoice.Performances);
+        }
+
+        [Fact]
+        public void SetCustomer_ShouldUpdateCustomerProperty()
+        {
+            var invoice = new Invoice("OldCo", new List<Performance>());
+
+            invoice.Customer = "NewCo";
+
+            Assert.Equal("NewCo", invoice.Customer);
+        }
+
+        [Fact]
+        public void SetPerformances_ShouldUpdatePerformancesProperty()
+        {
+            var invoice = new Invoice("BigCo", new List<Performance>());
+
+            var newPerformances = new List<Performance>
+            {
+                new Performance("hamlet", 55),
+                new Performance("as-like", 35)
+            };
+            invoice.Performances = newPerformances;
+
+            Assert.Equal(newPerformances, invoice.Performances);
+        }
+    }
+}
---
+++ TheatricalPlayersRefactoringKata.Tests/Models/PerformanceTests.cs
@@ -0,0 +1,40 @@
+﻿using Xunit;
+using TheatricalPlayersRefactoringKata.Models;
+
+namespace TheatricalPlayersRefactoringKata.Tests
+{
+    public class PerformanceTests
+    {
+        [Fact]
+        public void Constructor_ShouldInitializePropertiesCorrectly()
+        {
+            var playId = "hamlet";
+            var audience = 55;
+
+            var performance = new Performance(playId, audience);
+
+            Assert.Equal(playId, performance.PlayId);
+            Assert.Equal(audience, performance.Audience);
+        }
+
+        [Fact]
+        public void SetPlayId_ShouldUpdatePlayIdProperty()
+        {
+            var performance = new Performance("hamlet", 55);
+
+            performance.PlayId = "othello";
+
+            Assert.Equal("othello", performance.PlayId);
+        }
+
+        [Fact]
+        public void SetAudience_ShouldUpdateAudienceProperty()
+        {
+            var performance = new Performance("hamlet", 55);
+
+            performance.Audience = 100;
+
+            Assert.Equal(100, performance.Audience);
+        }
+    }
+}
