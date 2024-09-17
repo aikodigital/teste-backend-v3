0ac8c5a691af04506367998d2e264b39a203757e
Hilgo <lucas.stabile93@gmail.com>
2024-09-17 16:09:49 -0300

    Inclusão regra para peça history, teste funcionando

--- TheatricalPlayersRefactoringKata/StatementPrinter.cs
+++ TheatricalPlayersRefactoringKata/StatementPrinter.cs
@@ -33,6 +33,25 @@ public class StatementPrinter
                     }
                     thisAmount += 300 * perf.Audience;
                     break;
+                case "history":
+                    // Calcula o valor como tragédia
+                    var tragedyAmount = thisAmount;
+                    if (perf.Audience > 30)
+                    {
+                        tragedyAmount += 1000 * (perf.Audience - 30);
+                    }
+
+                    // Calcula o valor como comédia
+                    var comedyAmount = thisAmount;
+                    comedyAmount += 300 * perf.Audience;
+                    if (perf.Audience > 20)
+                    {
+                        comedyAmount += 10000 + 500 * (perf.Audience - 20);
+                    }
+
+                    // Valor total da peça histórica
+                    thisAmount = tragedyAmount + comedyAmount;
+                    break;
                 default:
                     throw new Exception("unknown type: " + play.Type);
             }
@@ -42,10 +61,10 @@ public class StatementPrinter
             if ("comedy" == play.Type) volumeCredits += (int)Math.Floor((decimal)perf.Audience / 5);
 
             // print line for this order
-            result += String.Format(cultureInfo, "  {0}: {1:C} ({2} seats)\n", play.Name, Convert.ToDecimal(thisAmount / 100), perf.Audience);
+            result += String.Format(cultureInfo, "  {0}: {1:C} ({2} seats)\n", play.Name, Convert.ToDecimal(thisAmount) / 100, perf.Audience);
             totalAmount += thisAmount;
         }
-        result += String.Format(cultureInfo, "Amount owed is {0:C}\n", Convert.ToDecimal(totalAmount / 100));
+        result += String.Format(cultureInfo, "Amount owed is {0:C}\n", Convert.ToDecimal(totalAmount) / 100);
         result += String.Format("You earned {0} credits\n", volumeCredits);
         return result;
     }
