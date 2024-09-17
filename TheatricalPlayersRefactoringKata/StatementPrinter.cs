014234b244bd40467bcfe34de11f01e216165b86
jheik <jheikalves7@gmail.com>
2024-09-16 23:03:09 -0300

    feat: adicionando novo gênero "history" com cálculo específico. fix: trocando tipo int para decimal por tratar de números com ponto flutuante.

--- TheatricalPlayersRefactoringKata/StatementPrinter.cs
+++ TheatricalPlayersRefactoringKata/StatementPrinter.cs
@@ -8,7 +8,7 @@ public class StatementPrinter
 {
     public string Print(Invoice invoice, Dictionary<string, Play> plays)
     {
-        var totalAmount = 0;
+        decimal totalAmount = 0;
         var volumeCredits = 0;
         var result = string.Format("Statement for {0}\n", invoice.Customer);
         CultureInfo cultureInfo = new CultureInfo("en-US");
@@ -19,19 +19,17 @@ public class StatementPrinter
             var lines = play.Lines;
             if (lines < 1000) lines = 1000;
             if (lines > 4000) lines = 4000;
-            var thisAmount = lines * 10;
+            decimal thisAmount = lines * 10;
             switch (play.Type) 
             {
                 case "tragedy":
-                    if (perf.Audience > 30) {
-                        thisAmount += 1000 * (perf.Audience - 30);
-                    }
+                    thisAmount = CalculateTragedyAmount(perf, thisAmount);
                     break;
                 case "comedy":
-                    if (perf.Audience > 20) {
-                        thisAmount += 10000 + 500 * (perf.Audience - 20);
-                    }
-                    thisAmount += 300 * perf.Audience;
+                    thisAmount = CalculateGenderComedy(perf, thisAmount);
+                    break;
+                case "history":
+                    thisAmount = CalculateTragedyAmount(perf, thisAmount) + CalculateGenderComedy(perf, thisAmount);
                     break;
                 default:
                     throw new Exception("unknown type: " + play.Type);
@@ -48,5 +46,29 @@ public class StatementPrinter
         result += String.Format(cultureInfo, "Amount owed is {0:C}\n", Convert.ToDecimal(totalAmount / 100));
         result += String.Format("You earned {0} credits\n", volumeCredits);
         return result;
+    }
+
+    private static decimal CalculateTragedyAmount(Performance perf, decimal thisAmount)
+    {
+        var baseAmount = thisAmount;
+        if (perf.Audience > 30)
+        {
+            baseAmount += 1000 * (perf.Audience - 30);
+        }
+
+        return baseAmount;
+    }
+
+    private static decimal CalculateGenderComedy(Performance perf, decimal thisAmount)
+    {
+        var baseAmount = thisAmount;
+        baseAmount += (300 * perf.Audience);
+
+        if (perf.Audience > 20)
+        {
+            baseAmount +=  10000 + (500 * (perf.Audience - 20));
+        }
+
+        return baseAmount;
     }
 }
