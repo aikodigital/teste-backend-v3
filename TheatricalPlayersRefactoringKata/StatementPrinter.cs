963d238c06de1aa40210d14bcfdf9a16a729baf0
ivanlmpk <ivanlempek@hotmail.com>
2024-09-17 17:06:57 -0300

    refactor: Alterado tipagem das variaveis totalAmount e thisAmount no método de Print para decimal. Ajustado os results que utilizam essas variaveis.

--- TheatricalPlayersRefactoringKata/StatementPrinter.cs
+++ TheatricalPlayersRefactoringKata/StatementPrinter.cs
@@ -8,7 +8,7 @@ public class StatementPrinter
 {
     public string Print(Invoice invoice, Dictionary<string, Play> plays)
     {
-        var totalAmount = 0;
+        decimal totalAmount = 0;
         var volumeCredits = 0;
         var result = string.Format("Statement for {0}\n", invoice.Customer);
         CultureInfo cultureInfo = new CultureInfo("en-US");
@@ -19,7 +19,7 @@ public class StatementPrinter
             var lines = play.Lines;
             if (lines < 1000) lines = 1000;
             if (lines > 4000) lines = 4000;
-            var thisAmount = lines * 10;
+            decimal thisAmount = lines * 10;
             switch (play.Type) 
             {
                 case "tragedy":
@@ -40,10 +40,11 @@ public class StatementPrinter
             if ("comedy" == play.Type) volumeCredits += (int)Math.Floor((decimal)perf.Audience / 5);
 
             // print line for this order
-            result += String.Format(cultureInfo, "  {0}: {1:C} ({2} seats)\n", play.Name, Convert.ToDecimal(thisAmount / 100), perf.Audience);
+            result += String.Format(cultureInfo, "  {0}: {1:C} ({2} seats)\n", play.Name, thisAmount / 100, perf.Audience);
+
             totalAmount += thisAmount;
         }
-        result += String.Format(cultureInfo, "Amount owed is {0:C}\n", Convert.ToDecimal(totalAmount / 100));
+        result += String.Format(cultureInfo, "Amount owed is {0:C}\n", totalAmount / 100);
         result += String.Format("You earned {0} credits\n", volumeCredits);
         return result;
     }
