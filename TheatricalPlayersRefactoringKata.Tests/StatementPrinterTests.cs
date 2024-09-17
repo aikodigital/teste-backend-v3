3964b94d37f75ce1820f7f08ff7cd8f991ffdb07
Gian Carlo Albino <giancarloalbino@hotmail.com>
2024-09-17 16:40:26 -0300

    Remoção de código descenessario, alteração do PlayType para um enum, alteração de valores em int para double, correção no calculo de valor base e novo PlayType History

--- TheatricalPlayersRefactoringKata.Tests/StatementPrinterTests.cs
+++ TheatricalPlayersRefactoringKata.Tests/StatementPrinterTests.cs
@@ -12,23 +12,22 @@ public class StatementPrinterTests
     [UseReporter(typeof(DiffReporter))]
     public void TestStatementExampleLegacy()
     {
-        var plays = new Dictionary<string, Play>();
-        plays.Add("hamlet", new Play("Hamlet", 4024, "tragedy"));
-        plays.Add("as-like", new Play("As You Like It", 2670, "comedy"));
-        plays.Add("othello", new Play("Othello", 3560, "tragedy"));
+        var plays = new Dictionary<string, Play>
+        {
+            { "hamlet", new Play("Hamlet", 4024, Play.TypePlay.Tragedy) },
+            { "as-like", new Play("As You Like It", 2670, Play.TypePlay.Comedy) },
+            { "othello", new Play("Othello", 3560, Play.TypePlay.Tragedy) }
+        };
 
-        Invoice invoice = new Invoice(
-            "BigCo",
-            new List<Performance>
-            {
-                new Performance("hamlet", 55),
-                new Performance("as-like", 35),
-                new Performance("othello", 40),
-            }
-        );
+        var invoice = new Invoice("BigCo", new List<Performance>
+        {
+            new ("hamlet", 55),
+            new ("as-like", 35),
+            new ("othello", 40)
+        });
 
-        StatementPrinter statementPrinter = new StatementPrinter();
-        var result = statementPrinter.Print(invoice, plays);
+        var statementPrinter = new StatementPrinter();
+        var result = StatementPrinter.Print(invoice, plays);
 
         Approvals.Verify(result);
     }
@@ -37,29 +36,28 @@ public class StatementPrinterTests
     [UseReporter(typeof(DiffReporter))]
     public void TestTextStatementExample()
     {
-        var plays = new Dictionary<string, Play>();
-        plays.Add("hamlet", new Play("Hamlet", 4024, "tragedy"));
-        plays.Add("as-like", new Play("As You Like It", 2670, "comedy"));
-        plays.Add("othello", new Play("Othello", 3560, "tragedy"));
-        plays.Add("henry-v", new Play("Henry V", 3227, "history"));
-        plays.Add("john", new Play("King John", 2648, "history"));
-        plays.Add("richard-iii", new Play("Richard III", 3718, "history"));
+        var plays = new Dictionary<string, Play>
+        {
+            { "hamlet", new Play("Hamlet", 4024, Play.TypePlay.Tragedy) },
+            { "as-like", new Play("As You Like It", 2670, Play.TypePlay.Comedy) },
+            { "othello", new Play("Othello", 3560, Play.TypePlay.Tragedy) },
+            { "henry-v", new Play("Henry V", 3227, Play.TypePlay.History) },
+            { "john", new Play("King John", 2648, Play.TypePlay.History) },
+            { "richard-iii", new Play("Richard III", 3718, Play.TypePlay.History) }
+        };
 
-        Invoice invoice = new Invoice(
-            "BigCo",
-            new List<Performance>
-            {
-                new Performance("hamlet", 55),
-                new Performance("as-like", 35),
-                new Performance("othello", 40),
-                new Performance("henry-v", 20),
-                new Performance("john", 39),
-                new Performance("henry-v", 20)
-            }
-        );
+        var invoice = new Invoice("BigCo", new List<Performance>
+        {
+            new ("hamlet", 55),
+            new ("as-like", 35),
+            new ("othello", 40),
+            new ("henry-v", 20),
+            new ("john", 39),
+            new ("richard-iii", 20)
+        });
 
-        StatementPrinter statementPrinter = new StatementPrinter();
-        var result = statementPrinter.Print(invoice, plays);
+        var statementPrinter = new StatementPrinter();
+        var result = StatementPrinter.Print(invoice, plays);
 
         Approvals.Verify(result);
     }
--- TheatricalPlayersRefactoringKata/Play.cs
+++ TheatricalPlayersRefactoringKata/Play.cs
@@ -4,15 +4,23 @@ public class Play
 {
     private string _name;
     private int _lines;
-    private string _type;
+    private TypePlay _type;
 
     public string Name { get => _name; set => _name = value; }
     public int Lines { get => _lines; set => _lines = value; }
-    public string Type { get => _type; set => _type = value; }
+    public TypePlay Type { get => _type; set => _type = value; }
 
-    public Play(string name, int lines, string type) {
-        this._name = name;
-        this._lines = lines;
-        this._type = type;
+    public Play(string name, int lines, TypePlay type) 
+    {
+        _name = name;
+        _lines = lines;
+        _type = type;
+    }
+
+    public enum TypePlay
+    {
+        Tragedy,
+        Comedy,
+        History,
     }
 }
