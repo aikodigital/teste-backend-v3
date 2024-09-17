6d8848525eb96d1502404fa56738fa36ced86abf
Hilgo <lucas.stabile93@gmail.com>
2024-09-17 18:09:01 -0300

    Incluído Método de exportação de dados em Formato XML

---
+++ TheatricalPlayersRefactoringKata/ItemData.cs
@@ -0,0 +1,14 @@
+﻿using System.Xml.Serialization;
+
+namespace TheatricalPlayersRefactoringKata
+{
+    [XmlType("Item")]
+    public class ItemData
+    {
+        [XmlIgnore]
+        public string PlayName { get; set; }
+        public decimal AmountOwed { get; set; }
+        public int EarnedCredits { get; set; }
+        public int Seats { get; set; }
+    }
+}
---
+++ TheatricalPlayersRefactoringKata/StatementData.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace TheatricalPlayersRefactoringKata
+{
+    [XmlRoot("Statement")]
+    public class StatementData
+    {
+        public string Customer { get; set; }
+
+        [XmlArray("Items")] 
+        [XmlArrayItem("Item")]
+        public List<ItemData> Items { get; set; } = new List<ItemData>();
+
+        [XmlElement("AmountOwed")]
+        public decimal TotalAmountOwed { get; set; }
+
+        [XmlElement("EarnedCredits")]
+        public int TotalEarnedCredits { get; set; }
+    }
+}
