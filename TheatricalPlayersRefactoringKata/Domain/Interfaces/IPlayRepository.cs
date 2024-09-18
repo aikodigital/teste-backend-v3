2b62cd96fcdf39d46119c78c5de267eb2266dc20
ivanlmpk <ivanlempek@hotmail.com>
2024-09-17 21:41:20 -0300

    feat: adicionado camada de interfaces com IPlayRepository e IPlayTypeCalculator implementando os contratos de calculos de valor e creditos além da busca por plays por id.

---
+++ TheatricalPlayersRefactoringKata/Domain/Interfaces/IPlayRepository.cs
@@ -0,0 +1,8 @@
+﻿using TheatricalPlayersRefactoringKata.Domain.Entities;
+
+namespace TheatricalPlayersRefactoringKata.Domain.Interfaces;
+
+public interface IPlayRepository
+{
+    Play GetPlayById(string playId);
+}
---
+++ TheatricalPlayersRefactoringKata/Domain/Interfaces/IPlayTypeCalculator.cs
@@ -0,0 +1,9 @@
+﻿using TheatricalPlayersRefactoringKata.Domain.Entities;
+
+namespace TheatricalPlayersRefactoringKata.Domain.Interfaces;
+
+public interface IPlayTypeCalculator
+{
+    public decimal CalculateAmount(Performance performance);
+    public int CalculateVolumeCredits(Performance performance);
+}
