8a6a23632d881cce8e0747312335a11a18906406
ivanlmpk <ivanlempek@hotmail.com>
2024-09-18 03:00:42 -0300

    feat: Adicionado projeto de API com dois end-points. Um para gerar o extrato e outro para adicionar ele a fila. Adicionado swagger. Ajustado launchsettings para iniciar a API na tela do swagger.

---
+++ TheatricalPlayersRefactoringKata.API/Controllers/StatementController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TheatricalPlayersRefactoringKata.Application.UseCases;
+using TheatricalPlayersRefactoringKata.Domain.Entities;
+
+namespace TheatricalPlayersRefactoringKata.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StatementController : ControllerBase
+    {
+        private readonly EnqueueStatementUseCase _enqueueStatementUseCase;
+        private readonly GenerateStatementUseCase _generateStatementUseCase;
+
+        public StatementController(EnqueueStatementUseCase enqueueStatementUseCase, GenerateStatementUseCase generateStatementUseCase)
+        {
+            _enqueueStatementUseCase = enqueueStatementUseCase;
+            _generateStatementUseCase = generateStatementUseCase;
+        }
+
+        [HttpPost("GenerateExtract")]
+        public IActionResult GenerateExtract([FromBody] Invoice invoice)
+        {
+            var response = _generateStatementUseCase.GenerateExtractValues(invoice);
+            return Ok(response);
+        }
+
+        [HttpPost("enqueue")]
+        public IActionResult EnqueueInvoice([FromBody] Invoice invoice)
+        {
+            _enqueueStatementUseCase.Execute(invoice);
+            return Accepted("Invoice enqueued for processing.");
+        }
+    }
+}
+
---
+++ TheatricalPlayersRefactoringKata.API/Program.cs
@@ -0,0 +1,48 @@
+using TheatricalPlayersRefactoringKata.Application.UseCases;
+using TheatricalPlayersRefactoringKata.Domain.Entities;
+using TheatricalPlayersRefactoringKata.Domain.Interfaces;
+using TheatricalPlayersRefactoringKata.Infrastructure.Queues;
+using TheatricalPlayersRefactoringKata.Infrastructure.Repositories;
+using TheatricalPlayersRefactoringKata.Presentation;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+builder.Services.AddSingleton(provider => new Dictionary<string, Play>
+{
+    { "hamlet", new Play("Hamlet", 4024, "tragedy") },
+    { "as-like", new Play("As You Like It", 2670, "comedy") },
+    { "othello", new Play("Othello", 3560, "tragedy") },
+    { "henry-v", new Play("Henry V", 3227, "history") },
+    { "richard-iii", new Play("Richard III", 3718, "history") }
+});
+
+builder.Services.AddSingleton<IPlayRepository, PlayRepository>();
+builder.Services.AddSingleton<IStatementQueue, StatementQueue>();
+
+builder.Services.AddTransient<GenerateStatementUseCase>();
+builder.Services.AddTransient<EnqueueStatementUseCase>();
+
+builder.Services.AddTransient<XmlStatementPrinter>();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
