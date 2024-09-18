7472a43439146560d8d3da77bcb0d14b53f40517
Fred Azzolini <fredazzolini.dev@gmail.com>
2024-09-18 05:18:41 -0300

    implement history

--- TheatricalPlayersRefactoringKata/StatementPrinter.cs
+++ TheatricalPlayersRefactoringKata/StatementPrinter.cs
@@ -6,6 +6,33 @@ namespace TheatricalPlayersRefactoringKata;
 
 public class StatementPrinter
 {
+
+    private static int CalculateAmount(string type, int baseValue, int audience)
+    {
+        switch (type)
+        {
+            case "tragedy":
+                return baseValue + 1000 * Math.Max(audience - 30, 0);
+            case "comedy":
+                return baseValue + 500 * Math.Max(audience - 20, 0) + 300 * audience + 10000 * (int)Math.Min(Math.Floor((double)(audience / 21)), 1);
+            case "history":
+                return 2 * baseValue + 1000 * Math.Max(audience - 30, 0) + 500 * Math.Max(audience - 20, 0) + 300 * audience + 10000 * (int)Math.Min(Math.Floor((double)(audience / 21)), 1);
+            default:
+                return 0;
+        }
+    }
+
+    private static int CalculateCredits(string type, int baseValue, int audience)
+    {
+        switch (type)
+        {
+            case "comedy":
+                return baseValue + (int)Math.Floor((decimal)audience / 5);
+            default:
+                return baseValue;
+        }
+    }
+
     public string Print(Invoice invoice, Dictionary<string, Play> plays)
     {
         var totalAmount = 0;
@@ -16,36 +43,26 @@ public class StatementPrinter
         foreach(var perf in invoice.Performances) 
         {
             var play = plays[perf.PlayId];
+            Console.WriteLine(string.Format("Performance of: {0}\n", play.Name));
             var lines = play.Lines;
             if (lines < 1000) lines = 1000;
             if (lines > 4000) lines = 4000;
+            Console.WriteLine(string.Format("Lines: {0}\n", lines));
+            Console.WriteLine(string.Format("audience: {0}\n", perf.Audience));
             var thisAmount = lines * 10;
-            switch (play.Type) 
-            {
-                case "tragedy":
-                    if (perf.Audience > 30) {
-                        thisAmount += 1000 * (perf.Audience - 30);
-                    }
-                    break;
-                case "comedy":
-                    if (perf.Audience > 20) {
-                        thisAmount += 10000 + 500 * (perf.Audience - 20);
-                    }
-                    thisAmount += 300 * perf.Audience;
-                    break;
-                default:
-                    throw new Exception("unknown type: " + play.Type);
-            }
+            Console.WriteLine(string.Format("thisAmount: {0}\n", thisAmount));
+            // calculate type credits and amount
+            thisAmount = CalculateAmount(play.Type, thisAmount, perf.Audience);
+            volumeCredits = CalculateCredits(play.Type, volumeCredits, perf.Audience);
             // add volume credits
             volumeCredits += Math.Max(perf.Audience - 30, 0);
-            // add extra credit for every ten comedy attendees
-            if ("comedy" == play.Type) volumeCredits += (int)Math.Floor((decimal)perf.Audience / 5);
 
             // print line for this order
-            result += String.Format(cultureInfo, "  {0}: {1:C} ({2} seats)\n", play.Name, Convert.ToDecimal(thisAmount / 100), perf.Audience);
+            result += String.Format(cultureInfo, "  {0}: {1:C} ({2} seats)\n", play.Name, Convert.ToDecimal(thisAmount) / 100, perf.Audience);
             totalAmount += thisAmount;
         }
-        result += String.Format(cultureInfo, "Amount owed is {0:C}\n", Convert.ToDecimal(totalAmount / 100));
+        Console.WriteLine(String.Format(cultureInfo, "Amount owed is {0:C}\n", Convert.ToDecimal(totalAmount) / 100));
+        result += String.Format(cultureInfo, "Amount owed is {0:C}\n", Convert.ToDecimal(totalAmount) / 100);
         result += String.Format("You earned {0} credits\n", volumeCredits);
         return result;
     }
