ead7e3990920c0c43090dd39b239bf5b652fee03
ivanlmpk <ivanlempek@hotmail.com>
2024-09-17 21:46:08 -0300

    feat: adicionado camada de serviços com classes para fazer os calculos dos generos de comédia, tragedia e história. Implementado também o design pattern de Factory Method para definir o tipo de calculo conforme o gênero.

---
+++ TheatricalPlayersRefactoringKata/Domain/Services/PlayTypeCalculators/TragedyCalculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using TheatricalPlayersRefactoringKata.Domain.Entities;
+using TheatricalPlayersRefactoringKata.Domain.Interfaces;
+
+namespace TheatricalPlayersRefactoringKata.Domain.Services.PlayTypeCalculators;
+
+public class TragedyCalculator : IPlayTypeCalculator
+{
+    public decimal CalculateAmount(Performance performance)
+    {
+        if (performance.Play == null)
+            throw new Exception("Play data is required for calculation.");
+
+        decimal amount = 0;
+        if (performance.Audience > 30)
+        {
+            amount += 1000 * (performance.Audience - 30);
+        }
+        return amount;
+    }
+
+    public int CalculateVolumeCredits(Performance performance)
+    {
+        return Math.Max(performance.Audience - 30, 0);
+    }
+}
