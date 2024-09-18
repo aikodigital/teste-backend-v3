8e71fede11f9366d27bffc46d5eead5f6113e892
ivanlmpk <ivanlempek@hotmail.com>
2024-09-17 21:50:48 -0300

    feat: Ajustado arquivo de teste para funcionar com refatorações. Adicionado teste para o XML. Ajustado primeira linha do arquivo de saida do XML.

--- TheatricalPlayersRefactoringKata.Tests/StatementPrinterTests.cs
+++ TheatricalPlayersRefactoringKata.Tests/StatementPrinterTests.cs
@@ -2,6 +2,12 @@ using System;
 using System.Collections.Generic;
 using ApprovalTests;
 using ApprovalTests.Reporters;
+using TheatricalPlayersRefactoringKata.Application.DTOs;
+using TheatricalPlayersRefactoringKata.Application.UseCases;
+using TheatricalPlayersRefactoringKata.Domain.Entities;
+using TheatricalPlayersRefactoringKata.Domain.Interfaces;
+using TheatricalPlayersRefactoringKata.Infrastructure.Repositories;
+using TheatricalPlayersRefactoringKata.Presentation;
 using Xunit;
 
 namespace TheatricalPlayersRefactoringKata.Tests;
@@ -27,8 +33,12 @@ public class StatementPrinterTests
             }
         );
 
+        IPlayRepository playRepository = new PlayRepository(plays);
+        var generateStatementUseCase = new GenerateStatementUseCase(playRepository);
         StatementPrinter statementPrinter = new StatementPrinter();
-        var result = statementPrinter.Print(invoice, plays);
+
+        StatementResult statementResult = generateStatementUseCase.GenerateExtractValues(invoice);
+        var result = statementPrinter.Print(statementResult);
 
         Approvals.Verify(result);
     }
@@ -58,8 +68,47 @@ public class StatementPrinterTests
             }
         );
 
-        StatementPrinter statementPrinter = new StatementPrinter();
-        var result = statementPrinter.Print(invoice, plays);
+        IPlayRepository playRepository = new PlayRepository(plays);
+        var generateStatementUseCase = new GenerateStatementUseCase(playRepository);
+        StatementPrinter statementPrinter= new StatementPrinter();
+
+        StatementResult statementResult = generateStatementUseCase.GenerateExtractValues(invoice);
+        var result = statementPrinter.Print(statementResult);
+
+        Approvals.Verify(result);
+    }
+
+    [Fact]
+    [UseReporter(typeof(DiffReporter))]
+    public void TestXmlStatementExample()
+    {
+        var plays = new Dictionary<string, Play>();
+        plays.Add("hamlet", new Play("Hamlet", 4024, "tragedy"));
+        plays.Add("as-like", new Play("As You Like It", 2670, "comedy"));
+        plays.Add("othello", new Play("Othello", 3560, "tragedy"));
+        plays.Add("henry-v", new Play("Henry V", 3227, "history"));
+        plays.Add("john", new Play("King John", 2648, "history"));
+        plays.Add("richard-iii", new Play("Richard III", 3718, "history"));
+
+        Invoice invoice = new Invoice(
+            "BigCo",
+            new List<Performance>
+            {
+                new Performance("hamlet", 55),
+                new Performance("as-like", 35),
+                new Performance("othello", 40),
+                new Performance("henry-v", 20),
+                new Performance("john", 39),
+                new Performance("henry-v", 20)
+            }
+        );
+
+        IPlayRepository playRepository = new PlayRepository(plays);
+        var generateStatementUseCase = new GenerateStatementUseCase(playRepository);
+        XmlStatementPrinter statementPrinterXml = new XmlStatementPrinter();
+
+        var statementResult = generateStatementUseCase.GenerateExtractValues(invoice);
+        var result = statementPrinterXml.PrintXml(statementResult);
 
         Approvals.Verify(result);
     }
