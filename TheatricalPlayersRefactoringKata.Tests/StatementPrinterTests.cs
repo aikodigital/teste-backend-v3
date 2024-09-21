46a57665db4320aa11ad5f4b7c092bf919fd75fc
André Cunha <andre.cunha@clickcompliance.com>
2024-09-21 14:26:12 -0300

    Refatorando classes StatementPrinterTests e Invoice

--- TheatricalPlayersRefactoringKata.Tests/StatementPrinterTests.cs
+++ TheatricalPlayersRefactoringKata.Tests/StatementPrinterTests.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using ApprovalTests;
 using ApprovalTests.Reporters;
 using Xunit;
@@ -8,27 +9,51 @@ namespace TheatricalPlayersRefactoringKata.Tests;
 
 public class StatementPrinterTests
 {
+    private readonly static string Hamlet = "hamlet";
+    private readonly static string AsLike = "as-like";
+    private readonly static string Othello = "othello";
+    private readonly static string HenryV = "henry-v";
+    private readonly static string John = "john";
+    private readonly static string RichardIii = "richard-iii";
+    private readonly static string Customer = "BigCo";
+
+    private readonly static Dictionary<string, Play> PlayMap = new()
+    {
+        { Hamlet, new Play("Hamlet", 4024, "tragedy") },
+        { AsLike, new Play("As You Like It", 2670, "comedy") },
+        { Othello, new Play("Othello", 3560, "tragedy") },
+        { HenryV, new Play("Henry V", 3227, "history") },
+        { John, new Play("King John", 2648, "history") },
+        { RichardIii, new Play("Richard III", 3718, "history") }
+    };
+
+    private readonly static List<Performance> Performances = new()
+    {
+        new (Hamlet, 55),
+        new (AsLike, 35),
+        new (Othello, 40),
+        new (HenryV, 20),
+        new (John, 39),
+        new (RichardIii, 20)
+    };
+
     [Fact]
     [UseReporter(typeof(DiffReporter))]
     public void TestStatementExampleLegacy()
     {
-        var plays = new Dictionary<string, Play>();
-        plays.Add("hamlet", new Play("Hamlet", 4024, "tragedy"));
-        plays.Add("as-like", new Play("As You Like It", 2670, "comedy"));
-        plays.Add("othello", new Play("Othello", 3560, "tragedy"));
+        var invoice = new Invoice
+        {
+            Customer = Customer,
+            Performances = Performances
+                .Take(3)
+                .ToList()
+        };
 
-        Invoice invoice = new Invoice(
-            "BigCo",
-            new List<Performance>
-            {
-                new Performance("hamlet", 55),
-                new Performance("as-like", 35),
-                new Performance("othello", 40),
-            }
-        );
-
-        StatementPrinter statementPrinter = new StatementPrinter();
-        var result = statementPrinter.Print(invoice, plays);
+        var result = new StatementPrinter().Print(
+            invoice,
+            PlayMap
+                .Take(3)
+                .ToDictionary(p => p.Key, p => p.Value));
 
         Approvals.Verify(result);
     }
@@ -37,29 +62,13 @@ public class StatementPrinterTests
     [UseReporter(typeof(DiffReporter))]
     public void TestTextStatementExample()
     {
-        var plays = new Dictionary<string, Play>();
-        plays.Add("hamlet", new Play("Hamlet", 4024, "tragedy"));
-        plays.Add("as-like", new Play("As You Like It", 2670, "comedy"));
-        plays.Add("othello", new Play("Othello", 3560, "tragedy"));
-        plays.Add("henry-v", new Play("Henry V", 3227, "history"));
-        plays.Add("john", new Play("King John", 2648, "history"));
-        plays.Add("richard-iii", new Play("Richard III", 3718, "history"));
+        var invoice = new Invoice
+        {
+            Customer = Customer,
+            Performances = Performances
+        };
 
-        Invoice invoice = new Invoice(
-            "BigCo",
-            new List<Performance>
-            {
-                new Performance("hamlet", 55),
-                new Performance("as-like", 35),
-                new Performance("othello", 40),
-                new Performance("henry-v", 20),
-                new Performance("john", 39),
-                new Performance("henry-v", 20)
-            }
-        );
-
-        StatementPrinter statementPrinter = new StatementPrinter();
-        var result = statementPrinter.Print(invoice, plays);
+        var result = new StatementPrinter().Print(invoice, PlayMap);
 
         Approvals.Verify(result);
     }
--- TheatricalPlayersRefactoringKata/Invoice.cs
+++ TheatricalPlayersRefactoringKata/Invoice.cs
@@ -4,16 +4,6 @@ namespace TheatricalPlayersRefactoringKata;
 
 public class Invoice
 {
-    private string _customer;
-    private List<Performance> _performances;
-
-    public string Customer { get => _customer; set => _customer = value; }
-    public List<Performance> Performances { get => _performances; set => _performances = value; }
-
-    public Invoice(string customer, List<Performance> performance)
-    {
-        this._customer = customer;
-        this._performances = performance;
-    }
-
+    public string Customer { get; set; }
+    public List<Performance> Performances { get; set; }
 }
