feb2f192f2c87e1dd8b17a30a54311e7522b63ca
João <Joo@DESKTOP-H8C8FCK>
2024-09-27 16:11:15 -0300

    updated invoice process

---
+++ TheatricalPlayersRefactoringKata.Domain/Entities/InvoiceProcess.cs
@@ -0,0 +1,8 @@
+﻿namespace TheatricalPlayersRefactoringKata.Domain.Entities;
+
+public class InvoiceProcess : BaseEntity
+{
+    public int InvoiceId { get; set; }
+    public bool Process { get; set; }
+    public virtual Invoice Invoice { get; set; }
+}
---
+++ TheatricalPlayersRefactoringKata.Infra.Data/Repositories/InvoiceProcessRepository.cs
@@ -0,0 +1,13 @@
+﻿using TheatricalPlayersRefactoringKata.Domain.Entities;
+using TheatricalPlayersRefactoringKata.Domain.Interfaces.Repositories;
+using TheatricalPlayersRefactoringKata.Infra.Data.Context;
+
+namespace TheatricalPlayersRefactoringKata.Infra.Data.Repositories;
+
+public class InvoiceProcessRepository : BaseRepository<InvoiceProcess, ApplicationDbContext>, IInvoiceProcessRepository
+{
+    public InvoiceProcessRepository(ApplicationDbContext context) : base(context)
+    {
+
+    }
+}
