87f408bd9c834d287eae962bea7a7f4b8dbd8f1a
rodrigoreis99 <rodrigo.reis.prado@gmail.com>
2024-10-01 17:22:32 -0300

    Criando impressão XML
    Criado impressão XML
    Criado testes para impressão XML

--- TheatricalPlayersRefactoringKata.Tests/StatementPrinterTests.cs
+++ TheatricalPlayersRefactoringKata.Tests/StatementPrinterTests.cs
@@ -12,20 +12,18 @@ public class StatementPrinterTests
     [UseReporter(typeof(DiffReporter))]
     public void TestStatementExampleLegacy()
     {
-        var plays = new Dictionary<string, Play>
-        {
-            { "hamlet", new("Hamlet", 4024, "tragedy") },
-            { "as-like", new("As You Like It", 2670, "comedy") },
-            { "othello", new("Othello", 3560, "tragedy") }
-        };
+        var plays = new Dictionary<string, Play>();
+        plays.Add("hamlet", new Play("Hamlet", 4024, "tragedy"));
+        plays.Add("as-like", new Play("As You Like It", 2670, "comedy"));
+        plays.Add("othello", new Play("Othello", 3560, "tragedy"));
 
         Invoice invoice = new Invoice(
             "BigCo",
             new List<Performance>
             {
-                new("hamlet", 55),
-                new("as-like", 35),
-                new("othello", 40),
+                new Performance("hamlet", 55),
+                new Performance("as-like", 35),
+                new Performance("othello", 40),
             }
         );
 
@@ -39,26 +37,24 @@ public class StatementPrinterTests
     [UseReporter(typeof(DiffReporter))]
     public void TestTextStatementExample()
     {
-        var plays = new Dictionary<string, Play>
-        {
-            { "hamlet", new Play("Hamlet", 4024, "tragedy") },
-            { "as-like", new Play("As You Like It", 2670, "comedy") },
-            { "othello", new Play("Othello", 3560, "tragedy") },
-            { "henry-v", new Play("Henry V", 3227, "history") },
-            { "john", new Play("King John", 2648, "history") },
-            { "richard-iii", new Play("Richard III", 3718, "history") }
-        };
+        var plays = new Dictionary<string, Play>();
+        plays.Add("hamlet", new Play("Hamlet", 4024, "tragedy"));
+        plays.Add("as-like", new Play("As You Like It", 2670, "comedy"));
+        plays.Add("othello", new Play("Othello", 3560, "tragedy"));
+        plays.Add("henry-v", new Play("Henry V", 3227, "history"));
+        plays.Add("john", new Play("King John", 2648, "history"));
+        plays.Add("richard-iii", new Play("Richard III", 3718, "history"));
 
         Invoice invoice = new Invoice(
             "BigCo",
             new List<Performance>
             {
-                new("hamlet", 55),
-                new("as-like", 35),
-                new("othello", 40),
-                new("henry-v", 20),
-                new("john", 39),
-                new("henry-v", 20)
+                new Performance("hamlet", 55),
+                new Performance("as-like", 35),
+                new Performance("othello", 40),
+                new Performance("henry-v", 20),
+                new Performance("john", 39),
+                new Performance("henry-v", 20)
             }
         );
 
@@ -67,4 +63,36 @@ public class StatementPrinterTests
 
         Approvals.Verify(result);
     }
+
+    [Fact]
+    [UseReporter(typeof(DiffReporter))]
+    public void TestXmlStatementExample()
+    {
+        var plays = new Dictionary<string, Play>();
+        plays.Add("hamlet", new Play("Hamlet", 4024, "tragedy"));
+        plays.Add("as-like", new Play("As You Like It", 2670, "comedy"));
+        plays.Add("othello", new Play("Othello", 3560, "tragedy"));
+        plays.Add("henry-v", new Play("Henry V", 3227, "history"));
+        plays.Add("john", new Play("King John", 2648, "history"));
+        plays.Add("richard-iii", new Play("Richard III", 3718, "history"));
+
+        Invoice invoice = new Invoice(
+            "BigCo",
+            new List<Performance>
+            {
+            new Performance("hamlet", 55),
+            new Performance("as-like", 35),
+            new Performance("othello", 40),
+            new Performance("henry-v", 20),
+            new Performance("john", 39),
+            new Performance("henry-v", 20)
+            }
+        );
+
+        XmlStatementPrinter xmlStatementPrinter = new XmlStatementPrinter();
+        var result = xmlStatementPrinter.Print(invoice, plays);
+
+        Approvals.Verify(result);
+    }
+
 }
---
+++ TheatricalPlayersRefactoringKata/XmlStatementPrinter.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace TheatricalPlayersRefactoringKata
+{
+    public class XmlStatementPrinter
+    {
+        public string Print(Invoice invoice, Dictionary<string, Play> plays)
+        {
+            decimal totalAmount = 0m;
+            decimal volumeCredits = 0;
+            StringBuilder result = new StringBuilder();
+            CultureInfo cultureInfo = new CultureInfo("en-US");
+
+            // Início do XML<?xml version="1.0" encoding="utf-8"?>
+            result.AppendLine("<?xml version=\"1.0\" encoding=\"utf-8\"?>");
+            result.AppendLine("<Statement xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">");
+            result.AppendLine($"  <Customer>{invoice.Customer}</Customer>");
+            result.AppendLine("  <Items>");
+
+            foreach (var perf in invoice.Performances)
+            {
+                var play = plays[perf.PlayId];
+
+                decimal lines = play.Lines;
+                lines = Math.Max(1000, Math.Min(lines, 4000));
+
+                decimal thisAmount = lines * 10;
+
+                switch (play.Type)
+                {
+                    case "tragedy":
+                        if (perf.Audience > 30)
+                        {
+                            thisAmount += 1000m * (perf.Audience - 30);
+                        }
+                        break;
+                    case "comedy":
+                        if (perf.Audience > 20)
+                        {
+                            thisAmount += 10000m + 500m * (perf.Audience - 20);
+                        }
+                        thisAmount += 300m * perf.Audience;
+                        break;
+                    case "history":
+                        decimal tragedyAmount = thisAmount;
+                        decimal comedyAmount = thisAmount + 300m * perf.Audience;
+
+                        if (perf.Audience > 30)
+                        {
+                            tragedyAmount += 1000m * (perf.Audience - 30);
+                        }
+                        if (perf.Audience > 20)
+                        {
+                            comedyAmount += 10000m + 500m * (perf.Audience - 20);
+                        }
+
+                        thisAmount = tragedyAmount + comedyAmount;
+                        break;
+                    default:
+                        throw new Exception("unknown type: " + play.Type);
+                }
+
+                // Adicionar volume credits
+                volumeCredits += Math.Max(perf.Audience - 30, 0);
+
+                // Adicionar extra credit para cada dez espectadores de comédia
+                if ("comedy" == play.Type)
+                    volumeCredits += (int)Math.Floor((decimal)perf.Audience / 5);
+
+                // Adicionar item ao XML
+                result.AppendLine("    <Item>");
+                result.AppendLine($"      <AmountOwed>{FormatAmount(thisAmount / 100)}</AmountOwed>");
+                result.AppendLine($"      <EarnedCredits>{(play.Type == "comedy" ? (int)Math.Floor((decimal)perf.Audience / 5) + Math.Max(perf.Audience - 30, 0) : Math.Max(perf.Audience - 30, 0))}</EarnedCredits>");
+                result.AppendLine($"      <Seats>{perf.Audience}</Seats>");
+                result.AppendLine("    </Item>");
+
+                totalAmount += thisAmount;
+            }
+
+            result.AppendLine("  </Items>");
+            result.AppendLine($"  <AmountOwed>{FormatAmount(totalAmount / 100)}</AmountOwed>");
+            result.AppendLine($"  <EarnedCredits>{volumeCredits}</EarnedCredits>");
+            result.AppendLine("</Statement>");
+
+            return result.ToString();
+        }
+
+        private string FormatAmount(decimal amount)
+        {
+            // Verifica se o valor é inteiro
+            return amount % 1 == 0 ? amount.ToString("0", CultureInfo.InvariantCulture) : amount.ToString("0.0", CultureInfo.InvariantCulture);
+        }
+    }
+}
