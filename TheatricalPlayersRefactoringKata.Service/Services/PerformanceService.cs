5e89387a4b5c7d869e384d2c286333e7be088ecc
Isabella Chiara <isabellacsiqueira96@gmail.com>
2024-11-11 11:42:10 -0300

    refac #4: adicionado PerformanceService para futura api

---
+++ TheatricalPlayersRefactoringKata.Service/Services/PerformanceService.cs
@@ -0,0 +1,39 @@
+using TheatricalPlayersRefactoringKata.Model;
+using TheatricalPlayersRefactoringKata.Repository;
+
+namespace TheatricalPlayersRefactoringKata.Service;
+
+public class PerformanceService
+{
+    private readonly PerfomanceRepository _performanceRepository;
+
+    public PerformanceService(PerfomanceRepository performanceRepository)
+    {
+        _performanceRepository = performanceRepository;
+    }
+
+    public List<Performance> GetAllPerformances()
+    {
+        return _performanceRepository.GetAll();
+    }
+
+    public bool AddPerformance(Performance perf)
+    {
+        return _performanceRepository.Add(perf);
+    }
+
+    public Performance? GetPerformanceById(int id)
+    {
+        return _performanceRepository.GetById(id);
+    }
+
+    public bool DeletePerformance(int id)
+    {
+        return _performanceRepository.Remove(id);
+    }
+
+    public bool UpdatePerformance(int id, Performance perf)
+    {
+        return _performanceRepository.Update(id, perf);
+    }
+}
