6620bf94f49fc71a973cb9a51209a9f030f4cc0a
André Lucas Silva Freires <andre.lucas@ed.company>
2024-11-22 00:02:59 -0400

    XmlInvoiceFormatter -> Definido namespaces
    StatementPrinterTests.TestXmlStatementExample.approved.txt -> havia alguma caractere estranho e oculto e imorrível rsrs

--- Aplication/Services/Formatters/XmlInvoiceFormatter.cs
+++ Aplication/Services/Formatters/XmlInvoiceFormatter.cs
@@ -1,6 +1,8 @@
 ﻿using Aplication.DTO;
 using Aplication.Interfaces;
 using System.Globalization;
+using System.Text;
+using System.Xml;
 using System.Xml.Linq;
 
 namespace Aplication.Services.Formatters
@@ -11,14 +13,20 @@ namespace Aplication.Services.Formatters
             IEnumerable<PerformanceResult> performances)
         {
             CultureInfo cultura = new CultureInfo("en-US");
+
+            XNamespace xsi = "http://www.w3.org/2001/XMLSchema-instance";
+            XNamespace xsd = "http://www.w3.org/2001/XMLSchema";
+
             var xml = new XElement("Statement",
+                new XAttribute(XNamespace.Xmlns + "xsi", xsi),
+                new XAttribute(XNamespace.Xmlns + "xsd", xsd),
                 new XElement("Customer", invoice.Customer),
                 new XElement("Items",
                     performances.Select(perf =>
                     new XElement("Item",
-                        new XElement("AmountOwed", 
-                            (perf.ValorPorPerformance % 100 == 0) 
-                                ? string.Format(cultura, "{0:N0}", Convert.ToDecimal(perf.ValorPorPerformance) / 100) 
+                        new XElement("AmountOwed",
+                            (perf.ValorPorPerformance % 100 == 0)
+                                ? string.Format(cultura, "{0:N0}", Convert.ToDecimal(perf.ValorPorPerformance) / 100)
                                 : string.Format(cultura, "{0:N1}", Convert.ToDecimal(perf.ValorPorPerformance) / 100)),
                         new XElement("EarnedCredits", perf.EarnedCredits),
                         new XElement("Seats", perf.Audience)
@@ -28,7 +36,33 @@ namespace Aplication.Services.Formatters
                 new XElement("EarnedCredits", performances.Sum(x => x.EarnedCredits))
             );
 
-            return xml.ToString();
+            var xmlDocument = new XDocument
+            (
+                new XDeclaration("1.0", "utf-8", ""),
+                xml
+            );
+            using Utf8StringWriter stringWriter = SetDefinitions(xmlDocument);
+            return stringWriter.ToString();
         }
+
+        private static Utf8StringWriter SetDefinitions(XDocument xmlDocument)
+        {
+            var stringWriter = new Utf8StringWriter();
+            using (var xmlWritter = XmlWriter.Create(stringWriter, new XmlWriterSettings
+            {
+                Encoding = Encoding.UTF8,
+                Indent = true,
+                OmitXmlDeclaration = false
+            }))
+            {
+                xmlDocument.WriteTo(xmlWritter);
+            }
+
+            return stringWriter;
+        }
+    }
+    public class Utf8StringWriter : StringWriter
+    {
+        public override Encoding Encoding => Encoding.UTF8;
     }
 }
